557b6e48d6a2b9c11eaa25455e1d10becd5b0ed7
Nam Nguyen <gdbtek@yahoo.com>
2014-03-25 01:44:24 -0400

    add redis cookbook

---
+++ cookbooks/redis/attributes/default.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+downloadURL='http://download.redis.io/releases/redis-stable.tar.gz'
+user='redis'
+
+etcInitFile='/etc/init/redis.conf'
+etcProfileFile='/etc/profile.d/redis.sh'
+
+installFolder='/opt/redis'
+installBinFolder="${installFolder}/bin"
+installConfigFolder="${installFolder}/config"
+installDataFolder="${installFolder}/data"
---
+++ cookbooks/redis/recipes/install.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function installDependencies()
+{
+    apt-get update
+
+    apt-get install -y build-essential
+    apt-get install -y curl
+}
+
+function install()
+{
+    local currentPath="$(pwd)"
+    local tempFolder="$(mktemp -d)"
+
+    rm -rf "${installFolder}"
+    mkdir -p "${installBinFolder}" "${installConfigFolder}" "${installDataFolder}"
+
+    curl -L "${downloadURL}" | tar xz --strip 1 -C "${tempFolder}"
+
+    addSystemUser "${user}"
+    cd "${tempFolder}"
+    make
+    find "${tempFolder}/src" -type f ! -name "*.sh" -perm -u+x -exec cp -f {} "${installBinFolder}" \;
+
+    rm -rf "${tempFolder}"
+    cd "${currentPath}"
+
+    echo "export PATH=\"${installBinFolder}:\$PATH\"" > "${etcProfileFile}"
+    cp -f "${appPath}/../files/upstart/redis.conf" "${etcInitFile}"
+    cp -f "${appPath}/../files/conf/redis.conf" "${installConfigFolder}"
+
+    start "$(getFileName "${etcInitFile}")"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+
+    header 'INSTALLING REDIS'
+
+    checkRequireRootUser
+    installDependencies
+    install
+}
+
+main "${@}"
