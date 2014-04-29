8477684b8cedd4c5f1a3e5a56f641f5e72b87083
Nam Nguyen <namnguye@adobe.com>
2014-04-29 15:45:57 -0700

    add start-go-agent script

--- cookbooks/go-server/recipes/install.bash
+++ cookbooks/go-server/recipes/install.bash
@@ -24,21 +24,15 @@ function install()
 
     rm -f "${serverPackageFile}" "${agentPackageFile}"
 
-    # http://www.thoughtworks.com/products/docs/go/current/help/admin_install_multiple_agents.html
-
-    local currentPath="$(pwd)"
+    # Only Create Go-Agent Folder Structure
 
     for ((i = 1; i <= numberOfAgent; i++))
     do
         local goAgentFolder="/var/lib/go-agent-${i}"
 
         mkdir -p "${goAgentFolder}" &&
-        chown -R 'go:go' "${goAgentFolder}" &&
-        cd "${goAgentFolder}" &&
-        sudo -u go nohup java -jar /usr/share/go-agent/agent-bootstrapper.jar 127.0.0.1 &
+        chown -R 'go:go' "${goAgentFolder}"
     done
-
-    cd "${currentPath}"
 }
 
 function main()
