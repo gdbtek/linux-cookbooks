e662d11bd2b6416de649bd4e44b2330b20335e80
Nam Nguyen <gdbtek@yahoo.com>
2014-05-01 12:10:22 -0700

    add Ruby cookbook

---
+++ cookbooks/ruby/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+downloadURL='http://cache.ruby-lang.org/pub/ruby/stable/ruby-2.1.1.tar.gz'
+
+installFolder='/opt/ruby'
---
+++ cookbooks/ruby/recipes/install.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function installDependencies()
+{
+    apt-get update
+
+    apt-get install -y build-essential
+}
+
+function install()
+{
+    # Clean Up
+
+    rm -rf "${installFolder}"
+    mkdir -p "${installFolder}"
+
+    # Install
+
+    local currentPath="$(pwd)"
+    local tempFolder="$(getTemporaryFolder)"
+
+    unzipRemoteFile "${downloadURL}" "${tempFolder}"
+    cd "${tempFolder}"
+    "${tempFolder}/configure" --prefix="${installFolder}"
+    make
+    make install
+    rm -rf "${tempFolder}"
+    cd "${currentPath}"
+
+    # Config Profile
+
+    local profileConfigData=('__INSTALL_FOLDER__' "${installFolder}")
+
+    createFileFromTemplate "${appPath}/../files/profile/ruby.sh" '/etc/profile.d/ruby.sh' "${profileConfigData[@]}"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+
+    header 'INSTALLING RUBY'
+
+    checkRequireRootUser
+
+    installDependencies
+    install
+}
+
+main "${@}"
