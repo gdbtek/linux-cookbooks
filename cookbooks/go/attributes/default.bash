c618c9b21cc97667e10d2b78ea788bbaa705f95d
Nam Nguyen <namnguye@adobe.com>
2014-05-05 00:28:39 -0700

    add go cookbook

---
+++ cookbooks/go/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+downloadURL='https://code.google.com/p/go/downloads/detail?name=go1.2.1.linux-amd64.tar.gz'
+
+installFolder='/opt/go'
---
+++ cookbooks/go/recipes/install.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function install()
+{
+    # Clean Up
+
+    rm -rf "${installFolder}"
+    mkdir -p "${installFolder}"
+
+    # Install
+
+    unzipRemoteFile "${downloadURL}" "${installFolder}"
+    chown -R "$(whoami)":"$(whoami)" "${installFolder}"
+    symlinkLocalBin "${installFolder}/bin"
+
+    # Config Profile
+
+    local profileConfigData=('__INSTALL_FOLDER__' "${installFolder}")
+
+    createFileFromTemplate "${appPath}/../files/profile/go.sh" '/etc/profile.d/go.sh' "${profileConfigData[@]}"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+
+    header 'INSTALLING GO'
+
+    checkRequireRootUser
+    install
+}
+
+main "${@}"
