22603720b3f2e0ea5f261c0e8b8fdc509885fbf1
Nam Nguyen <namnguye@adobe.com>
2014-05-05 20:01:23 -0700

    update mongodb version

--- cookbooks/mongodb/attributes/default.bash
+++ cookbooks/mongodb/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-downloadURL='https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.0.tgz'
+downloadURL='https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.1.tgz'
 
 installFolder='/opt/mongodb'
 installDataFolder='/opt/mongodb/data'
