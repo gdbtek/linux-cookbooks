ed8a728af10dc6b1bc03020c8b4f8e5151750586
Nam Nguyen <gdbtek@yahoo.com>
2014-06-01 01:05:44 -0700

    add jenkins role

--- roles/jenkins.bash
+++ roles/jenkins.bash
@@ -4,12 +4,9 @@ function main()
 {
     local appPath="$(cd "$(dirname "${0}")" && pwd)"
 
-    "${appPath}/essential.bash" || exit 1
+    "${appPath}/java-web.bash" || exit 1
 
-    "${appPath}/../cookbooks/jdk/recipes/install.bash" || exit 1
-    "${appPath}/../cookbooks/tomcat/recipes/install.bash" || exit 1
     "${appPath}/../cookbooks/jenkins/recipes/install.bash" || exit 1
-    "${appPath}/../cookbooks/nginx/recipes/install.bash" || exit 1
 }
 
 main
