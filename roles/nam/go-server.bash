1fa213dd5b194e3f274cf28d26462e8737e3ca2d
Nam Nguyen <namnguye@adobe.com>
2014-06-03 00:04:26 -0700

    mount hd

--- roles/nam/go-server.bash
+++ roles/nam/go-server.bash
@@ -11,6 +11,7 @@ function main()
     "${appPath}/../../cookbooks/aws-cli/recipes/install.bash" || exit 1
     "${appPath}/../../cookbooks/node-js/recipes/install.bash" || exit 1
     "${appPath}/../../cookbooks/nginx/recipes/install.bash" || exit 1
+    "${appPath}/../../cookbooks/mount-hd/recipes/install.bash" '/dev/sdb' '/opt/go' || exit 1
     "${appPath}/../../cookbooks/go-server/recipes/install.bash" || exit 1
     "${appPath}/../../cookbooks/ps1/recipes/install.bash" 'go' || exit 1
 }
