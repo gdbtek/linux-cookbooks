422f72687b24ca241fe476484361be9724f4cce6
Nam Nguyen <namnguye@adobe.com>
2014-06-16 20:31:39 -0700

    update versions

--- cookbooks/mysql/attributes/default.bash
+++ cookbooks/mysql/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-downloadURL='http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.17-linux-glibc2.5-x86_64.tar.gz'
+downloadURL='http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz'
 
 installFolder='/opt/mysql'
 
