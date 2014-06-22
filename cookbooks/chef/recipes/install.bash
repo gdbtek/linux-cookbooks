ca3d94c29b6fdc80c2552bf6fbc20a8080f8dbc0
Nam Nguyen <namnguye@adobe.com>
2014-06-21 21:39:32 -0700

    bug

--- cookbooks/chef/recipes/install.bash
+++ cookbooks/chef/recipes/install.bash
@@ -11,6 +11,7 @@ function main()
     local appPath="$(cd "$(dirname "${0}")" && pwd)"
 
     source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
 
     checkRequireDistributor
 
