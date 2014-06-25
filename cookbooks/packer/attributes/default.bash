220844a17bab920c176c086c509cac426ad5c24b
Nam Nguyen <namnguye@adobe.com>
2014-06-24 21:18:50 -0700

    add packer cookbook

---
+++ cookbooks/packer/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+packerDownloadURL='https://dl.bintray.com/mitchellh/packer/0.6.0_linux_amd64.zip'
+
+packerInstallFolder='/opt/packer'
---
+++ cookbooks/packer/recipes/install.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function install()
+{
+    # Clean Up
+
+    rm -rf "${packerInstallFolder}"
+    mkdir -p "${packerInstallFolder}"
+
+    # Install
+
+    unzipRemoteFile "${packerDownloadURL}" "${packerInstallFolder}"
+    chown -R "$(whoami)":"$(whoami)" "${packerInstallFolder}"
+
+    # Config Profile
+
+    local profileConfigData=('__INSTALL_FOLDER__' "${packerInstallFolder}")
+
+    createFileFromTemplate "${appPath}/../files/profile/packer.sh" '/etc/profile.d/packer.sh' "${profileConfigData[@]}"
+
+    # Display Version
+
+    info "\n$(packer version)"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+
+    checkRequireDistributor
+
+    header 'INSTALLING PACKER'
+
+    checkRequireRootUser
+
+    install
+    installCleanUp
+}
+
+main "${@}"
