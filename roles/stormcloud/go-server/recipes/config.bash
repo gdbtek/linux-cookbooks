44fc9f5b439306dff09f4d67f713d8000923a522
Nam Nguyen <namnguye@adobe.com>
2014-06-26 11:35:05 -0700

    bug

--- roles/stormcloud/go-server/recipes/config.bash
+++ roles/stormcloud/go-server/recipes/config.bash
@@ -4,7 +4,7 @@ function configInitDaemonControlTool()
 {
     if [[ "$(getMachineRelease)" = '13.10' ]]
     then
-        cp "${appPath}/../files/initctl" '/usr/local/bin'
+        cp "${appPath}/../files/initctl" '/usr/local/bin' &&
         chmod 755 '/usr/local/bin/initctl'
 
         appendToFileIfNotFound '/etc/sudoers' "^\s*go\s+ALL=\(ALL\)\s+NOPASSWD:ALL\s*$" 'go ALL=(ALL) NOPASSWD:ALL' 'true' 'false'
@@ -23,18 +23,18 @@ function configPackages()
 
 function configRootAuthorizedKeys()
 {
-    mkdir -p ~root/.ssh
-    chmod 700 ~root/.ssh
-    cp "${appPath}/../files/authorized_keys" ~root/.ssh
+    mkdir -p ~root/.ssh &&
+    chmod 700 ~root/.ssh &&
+    cp "${appPath}/../files/authorized_keys" ~root/.ssh &&
     chmod 600 ~root/.ssh/authorized_keys
 }
 
 function configGoAWS()
 {
-    mkdir -p ~go/.aws
-    chmod 700 ~go/.aws
-    touch ~go/.aws/config.json
-    chmod 600 ~go/.aws/config.json
+    mkdir -p ~go/.aws &&
+    chmod 700 ~go/.aws &&
+    touch ~go/.aws/config.json &&
+    chmod 600 ~go/.aws/config.json &&
     chown -R go:go ~go/.aws
 }
 
@@ -55,17 +55,17 @@ function configGoHomeDirectory()
 
 function configGoKnownHosts()
 {
-    mkdir -p ~go/.ssh
-    chmod 700 ~go/.ssh
-    cp "${appPath}/../files/known_hosts" ~go/.ssh
-    chmod 600 ~go/.ssh/known_hosts
+    mkdir -p ~go/.ssh &&
+    chmod 700 ~go/.ssh &&
+    cp "${appPath}/../files/known_hosts" ~go/.ssh &&
+    chmod 600 ~go/.ssh/known_hosts &&
     chown -R go:go ~go/.ssh
 }
 
 function configGoNPM()
 {
-    cp "${appPath}/../files/.npmrc" ~go
-    chmod 600 ~go/.npmrc
+    cp "${appPath}/../files/.npmrc" ~go &&
+    chmod 600 ~go/.npmrc &&
     chown go:go ~go/.npmrc
 }
 
