ff871d93bdac65342c2d3751782fdc8f4a1edb81
Nam Nguyen <namnguye@adobe.com>
2014-06-26 21:47:21 -0700

    update version

--- cookbooks/tomcat/attributes/default.bash
+++ cookbooks/tomcat/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tomcatDownloadURL='http://www.us.apache.org/dist/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8.tar.gz'
+tomcatDownloadURL='http://www.us.apache.org/dist/tomcat/tomcat-8/v8.0.9/bin/apache-tomcat-8.0.9.tar.gz'
 
 tomcatInstallFolder='/opt/tomcat'
 # tomcatJDKFolder='/opt/jdk'
