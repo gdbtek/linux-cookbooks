8793ea78b678f584f7a2ddd878826531423646a3
Nam Nguyen <namnguye@adobe.com>
2014-06-27 11:28:01 -0700

    add ubuntu to ps1

--- roles/stormcloud/go-server/recipes/agent.bash
+++ roles/stormcloud/go-server/recipes/agent.bash
@@ -8,7 +8,7 @@ function main()
 
     "${appPath}/../../../../cookbooks/go-server/recipes/install-agent.bash" 'go.adobecc.com' || exit 1
     "${appPath}/config.bash" || exit 1
-    "${appPath}/../../../../cookbooks/ps1/recipes/install.bash" 'go' || exit 1
+    "${appPath}/../../../../cookbooks/ps1/recipes/install.bash" 'go' 'ubuntu' || exit 1
 }
 
 main "${@}"
