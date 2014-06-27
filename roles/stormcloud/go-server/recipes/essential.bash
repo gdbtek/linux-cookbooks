17b7b1882e08c8ec6ae738cbe0515a77738c7ddc
Nam Nguyen <namnguye@adobe.com>
2014-06-27 16:20:53 -0700

    update roles

--- roles/stormcloud/go-server/recipes/essential.bash
+++ roles/stormcloud/go-server/recipes/essential.bash
@@ -6,8 +6,9 @@ function main()
 
     "${appPath}/../../../../cookbooks/mount-hd/recipes/install.bash" '/dev/sdb' '/opt/go-server' || exit 1
 
-    "${appPath}/../../../essential.bash" || exit 1
-
+    "${appPath}/../../../../cookbooks/essential/recipes/install.bash" || exit 1
+    "${appPath}/../../../../cookbooks/ntp/recipes/install.bash" || exit 1
+    "${appPath}/../../../../cookbooks/vim/recipes/install.bash" || exit 1
     "${appPath}/../../../../cookbooks/aws-cli/recipes/install.bash" || exit 1
     "${appPath}/../../../../cookbooks/jdk/recipes/install.bash" || exit 1
     "${appPath}/../../../../cookbooks/node-js/recipes/install.bash" || exit 1
