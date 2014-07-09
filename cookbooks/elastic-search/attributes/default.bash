b50196541fc8cbdc60ea2c17f0e0f7b3845b2c78
Nam Nguyen <namnguye@adobe.com>
2014-07-08 23:47:50 -0700

    add elastic-search cookbook

---
+++ cookbooks/elastic-search/attributes/default.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+elasticsearchDownloadURL='https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.tar.gz'
+
+elasticsearchInstallFolder='/opt/elastic-search'
+# elasticsearchJDKFolder='/opt/jdk'
+
+elasticsearchServiceName='elastic-search'
+
+elasticsearchUID='elastic-search'
+elasticsearchGID='elastic-search'
+
+elasticsearchHTTPPort=9200
---
+++ cookbooks/elastic-search/recipes/install.bash
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+function installDependencies()
+{
+    if [[ "$(existCommand 'java')" = 'false' ]]
+    then
+        "${appPath}/../../jdk/recipes/install.bash"
+    fi
+}
+
+function install()
+{
+    # Clean Up
+
+    rm -rf "${elasticsearchInstallFolder}"
+    mkdir -p "${elasticsearchInstallFolder}"
+
+    # Install
+
+    unzipRemoteFile "${elasticsearchDownloadURL}" "${elasticsearchInstallFolder}"
+
+    # Config Profile
+
+    local profileConfigData=('__INSTALL_FOLDER__' "${elasticsearchInstallFolder}")
+
+    createFileFromTemplate "${appPath}/../files/profile/elastic-search.sh" '/etc/profile.d/elastic-search.sh' "${profileConfigData[@]}"
+
+    # Config Upstart
+
+    if [[ "$(isEmptyString "${elasticsearchJDKFolder}")" = 'true' ]]
+    then
+        local elasticsearchJDKFolder="${jdkInstallFolder}"
+    fi
+
+    local upstartConfigData=(
+        '__INSTALL_FOLDER__' "${elasticsearchInstallFolder}"
+        '__JDK_FOLDER__' "${elasticsearchJDKFolder}"
+        '__UID__' "${elasticsearchUID}"
+        '__GID__' "${elasticsearchGID}"
+    )
+
+    createFileFromTemplate "${appPath}/../files/upstart/elastic-search.conf" "/etc/init/${elasticsearchServiceName}.conf" "${upstartConfigData[@]}"
+
+    # Start
+
+    addSystemUser "${elasticsearchUID}" "${elasticsearchGID}"
+    chown -R "${elasticsearchUID}":"${elasticsearchGID}" "${elasticsearchInstallFolder}"
+    start "${elasticsearchServiceName}"
+
+    # Display Version
+
+    info "\n$("${elasticsearchInstallFolder}/bin/elasticsearch" -v)"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+    source "${appPath}/../../jdk/attributes/default.bash" || exit 1
+
+    checkRequireDistributor
+
+    header 'INSTALLING ELASTIC SEARCH'
+
+    checkRequireRootUser
+    checkRequirePort "${elasticsearchHTTPPort}"
+
+    installDependencies
+    install
+    installCleanUp
+
+    displayOpenPorts
+}
+
+main "${@}"
