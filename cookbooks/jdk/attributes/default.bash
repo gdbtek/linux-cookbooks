7c7ed33c9e0849838e86b360a9d936149a291d36
Nam Nguyen <namnguye@adobe.com>
2014-07-09 20:16:43 -0700

    update download path

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-jdkDownloadURL='https://dl.dropboxusercontent.com/u/15529567/downloads/jdk-linux-x64.tar.gz'
+jdkDownloadURL='https://dl.dropboxusercontent.com/u/15529567/downloads/jdk/jdk-latest-linux-x64.tar.gz'
+# jdkDownloadURL='https://dl.dropboxusercontent.com/u/15529567/downloads/jdk/jdk-7u60-linux-x64.tar.gz'
 
 jdkInstallFolder='/opt/jdk'
