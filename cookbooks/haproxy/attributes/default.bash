d63aed395d5c5f4b452eeb551a9d82a6136ca9f3
Nam Nguyen <namnguye@adobe.com>
2014-07-12 13:24:19 -0700

    lazy comment

--- cookbooks/haproxy/attributes/default.bash
+++ cookbooks/haproxy/attributes/default.bash
@@ -14,11 +14,15 @@ haproxyPort=80
 haproxy_CPU='native'
 haproxy_TARGET='custom'
 
-    # haproxy_USE_ACCEPT4=1
+haproxy_PCREDIR='/opt/pcre'
+haproxy_PCRE_LIB="${haproxy_PCREDIR}/lib"
+haproxy_PCRE_INC=="${haproxy_PCREDIR}/include"
+
+# haproxy_USE_ACCEPT4=1
 haproxy_USE_CPU_AFFINITY=1
 haproxy_USE_CRYPT_H=1
 haproxy_USE_CTTPROXY=1
-    # haproxy_USE_DLMALLOC=1
+# haproxy_USE_DLMALLOC=1
 haproxy_USE_EPOLL=1
 haproxy_USE_FUTEX=1
 haproxy_USE_GETADDRINFO=1
@@ -29,7 +33,7 @@ haproxy_USE_LINUX_SPLICE=1
 haproxy_USE_LINUX_TPROXY=1
 haproxy_USE_MY_ACCEPT4=1
 haproxy_USE_MY_EPOLL=1
-    # haproxy_USE_MY_SPLICE=1
+# haproxy_USE_MY_SPLICE=1
 haproxy_USE_NETFILTER=1
 haproxy_USE_OPENSSL=1
 haproxy_USE_PCRE=1
@@ -43,7 +47,3 @@ haproxy_USE_TFO=1
 haproxy_USE_TPROXY=1
 haproxy_USE_VSYSCALL=1
 haproxy_USE_ZLIB=1
-
-haproxy_PCREDIR='/opt/pcre'
-haproxy_PCRE_LIB="${haproxy_PCREDIR}/lib"
-haproxy_PCRE_INC=="${haproxy_PCREDIR}/include"
