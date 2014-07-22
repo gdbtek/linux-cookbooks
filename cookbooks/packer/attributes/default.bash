0cd1fc6dea42b5213fb054e58397ad65980774b0
Nam Nguyen <namnguye@adobe.com>
2014-07-21 22:03:21 -0700

    update packer version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-packerDownloadURL='https://dl.bintray.com/mitchellh/packer/0.6.0_linux_amd64.zip'
+packerDownloadURL='https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip'
 
 packerInstallFolder='/opt/packer'
