2a0717387261aa090326b6acae4256a777713174
Nam Nguyen <namnguye@adobe.com>
2014-07-26 09:57:36 -0700

    add more vmware role

---
+++ roles/nam/vmware-server.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function main()
+{
+    local appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    "${appPath}/../../cookbooks/ps1/recipes/install.bash" 'nam' || exit 1
+}
+
+main "${@}"
