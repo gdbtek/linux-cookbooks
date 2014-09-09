6e1a39b4735db04c364f902aabda1cfaa2265aca
Nam Nguyen <namnguye@adobe.com>
2014-09-08 23:14:08 -0700

    remove newline

--- lib/util.bash
+++ lib/util.bash
@@ -388,7 +388,7 @@ function runAptGetUpdate()
 
     if [[ "${lastAptGetUpdate}" -gt "${updateInterval}" ]]
     then
-        info "\napt-get update"
+        info "apt-get update"
         apt-get update -m
     else
         local lastUpdate="$(date -u -d @"${lastAptGetUpdate}" +'%-Hh %-Mm %-Ss')"
