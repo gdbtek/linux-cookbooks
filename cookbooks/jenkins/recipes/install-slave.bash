c8b267ce1716abcf6cfe561f0179648f21c0bf81
Nam Nguyen <namnguye@adobe.com>
2014-09-10 01:16:10 -0700

    update slave recipe

--- cookbooks/jenkins/recipes/install-slave.bash
+++ cookbooks/jenkins/recipes/install-slave.bash
@@ -2,9 +2,9 @@
 
 function installDependencies()
 {
-    if [[ ! -f "${jenkinsTomcatInstallFolder}/bin/catalina.sh" ]]
+    if [[ "$(existCommand 'java')" = 'false' || ! -d "${jdkInstallFolder}" ]]
     then
-        "${appPath}/../../tomcat/recipes/install.bash"
+        "${appPath}/../../jdk/recipes/install.bash"
     fi
 }
 
@@ -36,52 +36,6 @@ function install()
         ln -s "${jenkinsInstallFolder}" "${jenkinsDefaultInstallFolder}"
         chown -R "${jenkinsUserName}:${jenkinsGroupName}" "${jenkinsDefaultInstallFolder}" "${jenkinsInstallFolder}"
     fi
-
-    # Config Profile
-
-    local profileConfigData=('__INSTALL_FOLDER__' "${jenkinsInstallFolder}")
-
-    createFileFromTemplate "${appPath}/../templates/default/jenkins.sh.profile" '/etc/profile.d/jenkins.sh' "${profileConfigData[@]}"
-
-    # Install
-
-    checkExistFolder "${jenkinsTomcatInstallFolder}/webapps"
-
-    local temporaryFile="$(getTemporaryFile)"
-
-    checkExistURL "${jenkinsDownloadURL}"
-    debug "\nDownloading '${jenkinsDownloadURL}' to '${temporaryFile}'"
-    curl -L "${jenkinsDownloadURL}" -o "${temporaryFile}"
-    chown "${jenkinsUserName}:${jenkinsGroupName}" "${temporaryFile}"
-    mv "${temporaryFile}" "${jenkinsTomcatInstallFolder}/webapps/${appName}.war"
-    sleep 60
-
-    # Display Version
-
-    info "\nVersion: $('java' -jar "${jenkinsTomcatInstallFolder}/webapps/${appName}/WEB-INF/jenkins-cli.jar" \
-                              -s "http://127.0.0.1:${jenkinsTomcatHTTPPort}/${appName}" \
-                              version)"
-
-    # Refresh Update Center
-
-    if [[ "${jenkinsUpdateAllPlugins}" = 'false' && ${#jenkinsInstallPlugins[@]} -lt 1 ]]
-    then
-        "${appPath}/refresh-master-update-center.bash"
-    fi
-
-    # Update Plugins
-
-    if [[ "${jenkinsUpdateAllPlugins}" = 'true' ]]
-    then
-        "${appPath}/update-master-plugins.bash"
-    fi
-
-    # Install Plugins
-
-    if [[ ${#jenkinsInstallPlugins[@]} -gt 0 ]]
-    then
-        "${appPath}/install-master-plugins.bash" "${jenkinsInstallPlugins[@]}"
-    fi
 }
 
 function main()
@@ -89,12 +43,12 @@ function main()
     appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     source "${appPath}/../../../lib/util.bash"
-    source "${appPath}/../attributes/default.bash"
+    source "${appPath}/../attributes/master.bash"
 
     checkRequireSystem
     checkRequireRootUser
 
-    header 'INSTALLING MASTER JENKINS'
+    header 'INSTALLING SLAVE JENKINS'
 
     installDependencies
     install
