3e1fa90e631211f55dd2fe22e722c1d35d99a6c5
Nam Nguyen <namnguye@adobe.com>
2014-09-18 21:01:00 -0700

    add selenium-server cookbook

---
+++ cookbooks/selenium-server/recipes/install.bash
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    if [[ "$(existCommand 'java')" = 'false' || ! -d "${seleniumserverJDKInstallFolder}" ]]
+    then
+        "${appPath}/../../jdk/recipes/install.bash" "${seleniumserverJDKInstallFolder}"
+    fi
+}
+
+function install()
+{
+    # Clean Up
+
+    initializeFolder "${seleniumserverInstallFolder}"
+
+    # Install
+
+    unzipRemoteFile "${seleniumserverDownloadURL}" "${seleniumserverInstallFolder}"
+
+    # Add User
+
+    addUser "${seleniumserverUserName}" "${seleniumserverGroupName}" 'false' 'true' 'false'
+
+    # Config Upstart
+
+    local upstartConfigData=(
+        '__INSTALL_FOLDER__' "${seleniumserverInstallFolder}"
+        '__JDK_INSTALL_FOLDER__' "${seleniumserverJDKInstallFolder}"
+        '__USER_NAME__' "${seleniumserverUserName}"
+        '__GROUP_NAME__' "${seleniumserverGroupName}"
+    )
+
+    createFileFromTemplate "${appPath}/../templates/default/selenium-server.conf.upstart" "/etc/init/${seleniumserverServiceName}.conf" "${upstartConfigData[@]}"
+
+    # Start
+
+    chown -R "${seleniumserverUserName}:${seleniumserverGroupName}" "${seleniumserverInstallFolder}"
+    start "${seleniumserverServiceName}"
+
+    # Display Version
+
+    info "\n$("${seleniumserverInstallFolder}/bin/version.sh")"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appPath}/../../../libraries/util.bash"
+    source "${appPath}/../attributes/default.bash"
+
+    checkRequireSystem
+    checkRequireRootUser
+
+    header 'INSTALLING SELENIUM SERVER'
+
+    checkRequirePort "${seleniumserverAJPPort}" "${seleniumserverPort}"
+
+    installDependencies
+    install
+    installCleanUp
+
+    displayOpenPorts
+}
+
+main "${@}"
