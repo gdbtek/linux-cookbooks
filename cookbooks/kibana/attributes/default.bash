246a24db2e6792a68836900967d6ef7169330a7b
Nam Nguyen <namnguye@adobe.com>
2014-10-06 11:28:35 -0700

    update Kibana version

--- cookbooks/kibana/attributes/default.bash
+++ cookbooks/kibana/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-kibanaDownloadURL='https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz'
+kibanaDownloadURL='https://download.elasticsearch.org/kibana/kibana/kibana-3.1.1.tar.gz'
 kibanaElasticSearchURL='http://127.0.0.1:9200'
 
 kibanaInstallFolder='/opt/nginx/html'
