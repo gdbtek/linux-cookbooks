314df3bc43390b9f33872f5971ef4a961535ac45
Nam Nguyen <namnguye@adobe.com>
2014-10-06 22:02:00 -0700

    add my instance

---
+++ roles/ccmui/roles/nam/attributes/default.bash
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+ccmuiNamDisk='/dev/sdb'
+ccmuiNamMountOn='/opt'
+
+ccmuiNamGITUserName='Nam Nguyen'
+ccmuiNamGITUserEmail='namnguye@adobe.com'
---
+++ roles/ccmui/roles/nam/recipes/install.bash
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+function main()
+{
+    # Load Libraries
+
+    local appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appPath}/../../../../../cookbooks/mount-hd/attributes/default.bash"
+    source "${appPath}/../../../../../libraries/util.bash"
+    source "${appPath}/../../../libraries/util.bash"
+    source "${appPath}/../attributes/default.bash"
+
+    # Extend HD
+
+    extendOPTPartition "${ccmuiNamDisk}" "${ccmuiNamMountOn}" "${mounthdPartitionNumber}"
+
+    # Install Apps
+
+    "${appPath}/../../../../essential.bash"
+    "${appPath}/../../../../../cookbooks/aws-cli/recipes/install.bash"
+    "${appPath}/../../../../../cookbooks/chef/recipes/install.bash"
+    "${appPath}/../../../../../cookbooks/jdk/recipes/install.bash"
+    "${appPath}/../../../../../cookbooks/maven/recipes/install.bash"
+    "${appPath}/../../../../../cookbooks/node-js/recipes/install.bash"
+    "${appPath}/../../../../../cookbooks/packer/recipes/install.bash"
+    "${appPath}/../../../../../cookbooks/ruby/recipes/install.bash"
+
+    # Config SSH and GIT
+
+    addUserAuthorizedKey "$(whoami)" "$(whoami)" "$(cat "${appPath}/../files/default/authorized_keys")"
+    addUserSSHKnownHost "$(whoami)" "$(whoami)" "$(cat "${appPath}/../files/default/known_hosts")"
+
+    configUserGIT "$(whoami)" "${ccmuiNamGITUserName}" "${ccmuiNamGITUserEmail}"
+    generateUserSSHKey "$(whoami)"
+
+    # Clean Up
+
+    cleanUpSystemFolders
+    cleanUpITMess
+
+    # Display Notice
+
+    displayNotice "$(whoami)"
+}
+
+main "${@}"
