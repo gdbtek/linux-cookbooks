8b0b63b832e709046245846f73e1a515587e7ff1
Nam Nguyen <gdbtek@yahoo.com>
2014-10-27 08:44:12 -0700

    update

---
+++ cookbooks/shell-check/attributes/default.bash
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+cabalInstallFolder='/opt/cabal'
---
+++ cookbooks/shell-check/recipes/install.bash
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    installAptGetPackages 'cabal-install'
+}
+
+function install()
+{
+    cabal update
+    cabal install 'shellcheck'
+
+    mv ~/.cabal "${cabalInstallFolder}"
+
+    # Config Profile
+
+    local profileConfigData=('__INSTALL_FOLDER__' "${cabalInstallFolder}")
+
+    createFileFromTemplate "${appPath}/../templates/default/cabal.sh.profile" '/etc/profile.d/cabal.sh' "${profileConfigData[@]}"
+
+    # Display Version
+
+    info "$(shellcheck -v)"
+}
+
+function main()
+{
+    local appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appPath}/../../../libraries/util.bash"
+
+    checkRequireSystem
+    checkRequireRootUser
+
+    header 'INSTALLING SHELL-CHECK'
+
+    installDependencies
+    install
+    installCleanUp
+}
+
+main "${@}"
