d1f7ae852b6f63ae969e262a68b5fa0cde471325
Nam Nguyen <gdbtek@yahoo.com>
2014-10-28 14:26:57 -0700

    update versions

--- cookbooks/ruby/attributes/default.bash
+++ cookbooks/ruby/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-rubyDownloadURL='http://cache.ruby-lang.org/pub/ruby/stable/ruby-2.1.3.tar.gz'
+rubyDownloadURL='http://cache.ruby-lang.org/pub/ruby/stable/ruby-2.1.4.tar.gz'
 
 rubyInstallFolder='/opt/ruby'
