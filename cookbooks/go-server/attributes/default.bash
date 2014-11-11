e00ef3e8811c3e591123c7eb62a6f26427cc91c2
Nam Nguyen <gdbtek@yahoo.com>
2014-11-11 09:54:18 -0800

    update go-server version

--- cookbooks/go-server/attributes/default.bash
+++ cookbooks/go-server/attributes/default.bash
@@ -2,8 +2,8 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-goserverServerDownloadURL='http://download.go.cd/gocd/go-server-14.2.0-377.zip'
-goserverAgentDownloadURL='http://download.go.cd/gocd/go-agent-14.2.0-377.zip'
+goserverServerDownloadURL='http://download.go.cd/gocd/go-server-14.3.0-1186.zip'
+goserverAgentDownloadURL='http://download.go.cd/gocd/go-agent-14.3.0-1186.zip'
 
 goserverServerInstallFolder='/opt/go-server/server'
 goserverAgentInstallFolder='/opt/go-server/agents/agent'
