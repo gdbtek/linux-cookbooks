e5d7c9bc0d401499f9eeeadf3fb2781b042a818c
Nam Nguyen <gdbtek@yahoo.com>
2014-11-13 09:53:27 -0800

    update Ruby version

--- cookbooks/ruby/attributes/default.bash
+++ cookbooks/ruby/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-rubyDownloadURL='http://cache.ruby-lang.org/pub/ruby/stable/ruby-2.1.4.tar.gz'
+rubyDownloadURL='http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.5.tar.gz'
 
 rubyInstallFolder='/opt/ruby'
