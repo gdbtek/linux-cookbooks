cf3fcd16606a26be15f87e9c4d8ee8d5b7500958
Nam Nguyen <gdbtek@yahoo.com>
2014-12-11 20:12:52 -0800

    update go-lang version

--- cookbooks/go-lang/attributes/default.bash
+++ cookbooks/go-lang/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-golangDownloadURL='https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz'
+golangDownloadURL='https://storage.googleapis.com/golang/go1.4.linux-amd64.tar.gz'
 
 golangInstallFolder='/opt/go-lang'
