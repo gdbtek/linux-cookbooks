18669a23ee398134560a97865e778bdb76da928a
Nam Nguyen <gdbtek@yahoo.com>
2014-12-19 14:42:12 -0800

    update Maven version

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-mavenDownloadURL='http://www.us.apache.org/dist/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz'
+mavenDownloadURL='http://www.us.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz'
 
 mavenInstallFolder='/opt/maven'
 mavenJDKInstallFolder="${jdkInstallFolder}"
