0eb39cb23535200229e1d1093500a1aea5daa3e4
Nam Nguyen <gdbtek@yahoo.com>
2015-01-19 15:02:44 -0800

    add beautify script

---
+++ bin/beautify-bash.bash
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+function removeNewlineAtEndOfFile()
+{
+    local repositoryFolderPath="${1}"
+
+    find \
+        "${repositoryFolderPath}" \
+        -type f \
+        \( \
+            -name 'authorized_keys' -o \
+            -name 'known_hosts' -o \
+            -name "*.bash" -o \
+            -name "*.conf" -o \
+            -name "*.profile" -o \
+            -name "*.upstart" \
+        \) \
+        \( \
+            -not -path "${repositoryFolderPath}/.git/*" \
+        \) \
+        -exec bash -c -e '
+            for file
+            do
+                printf '%s' "$(< "${file}")" > "${file}"
+            done' bash '{}' \;
+}
+
+function main()
+{
+    local repositoryFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+    removeNewlineAtEndOfFile "${repositoryFolderPath}"
+}
+
+main "${@}"
