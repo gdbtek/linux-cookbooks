a98b03c96d6dd87969575fdad98c89f9ab7ab7fd
Nam Nguyen <gdbtek@yahoo.com>
2015-02-24 10:09:10 -0800

    update Tomcat

--- cookbooks/tomcat/attributes/default.bash
+++ cookbooks/tomcat/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export tomcatDownloadURL='http://www.us.apache.org/dist/tomcat/tomcat-8/v8.0.18/bin/apache-tomcat-8.0.18.tar.gz'
+export tomcatDownloadURL='http://www.us.apache.org/dist/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.tar.gz'
 
 export tomcatInstallFolder='/opt/tomcat'
 export tomcatJDKInstallFolder="${jdkInstallFolder}"
