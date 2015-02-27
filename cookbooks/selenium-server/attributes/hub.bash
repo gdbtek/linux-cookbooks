c1ae686c05ac1fa7bcb10d04aa7f1c1186af0af5
Nam Nguyen <gdbtek@yahoo.com>
2015-02-27 11:21:50 -0800

    update version

--- cookbooks/selenium-server/attributes/hub.bash
+++ cookbooks/selenium-server/attributes/hub.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export seleniumserverDownloadURL='http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar'
+export seleniumserverDownloadURL='http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar'
 
 export seleniumserverInstallFolder='/opt/selenium-server/hub'
 export seleniumserverJDKInstallFolder="${jdkInstallFolder}"
