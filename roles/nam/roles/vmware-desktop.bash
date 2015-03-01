29bf9d79242065b4749b8a1303e153767bfe5d3d
Nam Nguyen <gdbtek@yahoo.com>
2015-03-01 00:42:45 -0800

    update

--- roles/nam/roles/vmware-desktop.bash
+++ roles/nam/roles/vmware-desktop.bash
@@ -5,11 +5,13 @@ function main()
     local appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     source "${appPath}/../../../libraries/util.bash"
+    source "${appPath}/../libraries/util.bash"
 
     "${appPath}/../../../cookbooks/essential/recipes/install.bash"
     "${appPath}/../../../cookbooks/ps1/recipes/install.bash" --users 'nam'
     "${appPath}/../../../cookbooks/vim/recipes/install.bash"
 
+    setupGIT
     cleanUpSystemFolders
 }
 
--- roles/nam/roles/vmware-server.bash
+++ roles/nam/roles/vmware-server.bash
@@ -5,9 +5,11 @@ function main()
     local appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     source "${appPath}/../../../libraries/util.bash"
+    source "${appPath}/../libraries/util.bash"
 
     "${appPath}/../../../cookbooks/ps1/recipes/install.bash" --users 'nam'
 
+    setupGIT
     cleanUpSystemFolders
 }
 
