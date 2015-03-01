5855337014e073c1e53ea505e5377cc9a6d85883
Nam Nguyen <gdbtek@yahoo.com>
2015-02-28 22:33:13 -0800

    restore PS1

--- cookbooks/ps1/recipes/install.bash
+++ cookbooks/ps1/recipes/install.bash
@@ -8,15 +8,13 @@ function displayUsage()
     echo    "SYNOPSIS :"
     echo    "    ${scriptName}"
     echo    "        --help"
-    echo    "        --profile-file-name    <PROFILE_FILE_NAME>"
-    echo    "        --host-name            <HOST_NAME>"
-    echo    "        --users                <USERS>"
+    echo    "        --host-name    <HOST_NAME>"
+    echo    "        --users        <USERS>"
     echo -e "\033[1;35m"
     echo    "DESCRIPTION :"
-    echo    "    --help                 Help page"
-    echo    "    --profile-file-name    Profile file name such as '.profile', '.bash_profile', '.bashrc' (optional)"
-    echo    "    --host-name            Custom host name (optional). Default to current host name"
-    echo    "    --users                List of users separated by commas or spaces (optional). Default to current user"
+    echo    "    --help         Help page"
+    echo    "    --host-name    Custom host name (optional). Default to current host name"
+    echo    "    --users        List of users separated by commas or spaces (optional). Default to current user"
     echo -e "\033[1;36m"
     echo    "EXAMPLES :"
     echo    "    ./${scriptName} --help"
@@ -28,10 +26,6 @@ function displayUsage()
     echo    "    ./${scriptName}"
     echo    "        --host-name 'my-server.com'"
     echo    "        --users 'user1 user2 user3'"
-    echo    "    ./${scriptName}"
-    echo    "        --host-name 'my-server.com'"
-    echo    "        --users 'user1 user2 user3'"
-    echo    "        --profile-file-name '.bash_profile'"
     echo -e "\033[0m"
 
     exit "${1}"
@@ -39,9 +33,8 @@ function displayUsage()
 
 function install()
 {
-    local profileFileName="${1}"
-    local hostName="${2}"
-    local users=(${3//,/ } "$(whoami)")
+    local hostName="${1}"
+    local users=(${2//,/ } "$(whoami)")
 
     # Reformat PS1
 
@@ -57,14 +50,9 @@ function install()
 
     for user in "${users[@]}"
     do
-        local profileFilePath="$(getUserHomeFolder "${user}")/${profileFileName}"
+        local profileFile="$(getProfileFile "${user}")"
 
-        if [[ "$(isEmptyString "${profileFilePath}")" = 'false' ]]
-        then
-            local profileFilePath="$(getProfileFile "${user}")"
-        fi
-
-        if [[ "$(isEmptyString "${profileFilePath}")" = 'false' ]]
+        if [[ "$(isEmptyString "${profileFile}")" = 'false' ]]
         then
             if [[ "$(whoami)" = "${user}" ]]
             then
@@ -73,12 +61,12 @@ function install()
                 local prompt="export PS1=\"${ps1UserPrompt}\""
             fi
 
-            echo -e "Updating '\033[1;32m${profileFilePath}\033[0m'"
+            echo -e "Updating '\033[1;32m${profileFile}\033[0m'"
 
-            if [[ ! -f "${profileFilePath}" ]]
+            if [[ ! -f "${profileFile}" ]]
             then
-                touch "${profileFilePath}"
-                chown "${user}:${user}" "${profileFilePath}"
+                touch "${profileFile}"
+                chown "${user}:${user}" "${profileFile}"
             fi
 
             appendToFileIfNotFound "${profileFile}" "${prompt}" "${prompt}" 'false' 'false' 'true'
@@ -100,16 +88,6 @@ function main()
         case "${1}" in
             --help)
                 displayUsage 0
-                ;;
-
-            --profile-file-name)
-                shift
-
-                if [[ "${#}" -gt '0' ]]
-                then
-                    local profileFileName="$(trimString "${1}")"
-                fi
-
                 ;;
 
             --host-name)
@@ -143,7 +121,7 @@ function main()
 
     header 'INSTALLING PS1'
 
-    install "${profileFileName}" "${hostName}" "${users}"
+    install "${hostName}" "${users}"
     installCleanUp
 }
 
