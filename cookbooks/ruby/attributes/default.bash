48808f2c323000301fc1b0c6113e3f2226e2be35
Nam Nguyen <gdbtek@yahoo.com>
2015-03-03 19:37:56 -0800

    update ruby version

--- cookbooks/ruby/attributes/default.bash
+++ cookbooks/ruby/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export rubyDownloadURL='http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz'
+export rubyDownloadURL='http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.1.tar.gz'
 
 export rubyInstallFolder='/opt/ruby'
