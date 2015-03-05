27b75b808eb51ec7ea46bfa2a7c2a75352ad5ef3
Nam Nguyen <gdbtek@yahoo.com>
2015-03-04 23:53:07 -0800

    update kibana version

--- cookbooks/kibana/attributes/default.bash
+++ cookbooks/kibana/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export kibanaDownloadURL='https://download.elasticsearch.org/kibana/kibana/kibana-4.0.0-linux-x64.tar.gz'
+export kibanaDownloadURL='https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz'
 export kibanaElasticSearchURL='http://127.0.0.1:9200'
 
 export kibanaInstallFolder='/opt/nginx/html'
