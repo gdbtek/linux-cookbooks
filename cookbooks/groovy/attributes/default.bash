c80d198509d35a879db4ce8e5c20180dfbb97948
Nam Nguyen <gdbtek@yahoo.com>
2015-03-19 10:35:11 -0700

    update maven and groovy

--- cookbooks/groovy/attributes/default.bash
+++ cookbooks/groovy/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export groovyDownloadURL='http://dl.bintray.com/groovy/maven/groovy-binary-2.4.1.zip'
+export groovyDownloadURL='http://dl.bintray.com/groovy/maven/groovy-binary-2.4.2.zip'
 
 export groovyInstallFolder='/opt/groovy'
 export groovyJDKInstallFolder="${jdkInstallFolder}"
--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export mavenDownloadURL='http://www.us.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz'
+export mavenDownloadURL='http://www.us.apache.org/dist/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz'
 
 export mavenInstallFolder='/opt/maven'
 export mavenJDKInstallFolder="${jdkInstallFolder}"
