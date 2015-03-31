4c80017831d457099fedd0d04d945b72b5a6f5b5
Nam Nguyen <gdbtek@yahoo.com>
2015-03-31 15:47:56 -0700

    bug fixes

--- cookbooks/ec2-ami-tools/recipes/install.bash
+++ cookbooks/ec2-ami-tools/recipes/install.bash
@@ -8,10 +8,28 @@ function install()
 
     # Install
 
-    local tempFolder="$(getTemporaryFolder)"
+    unzipRemoteFile "${ec2amitoolsDownloadURL}" "${awscliInstallFolder}"
 
-    unzipRemoteFile "${ec2amitoolsDownloadURL}" "${tempFolder}"
-    # rm -f -r "${tempFolder}"
+    local unzipFolder="$(find "${awscliInstallFolder}" -maxdepth 1 -xtype d 2> '/dev/null' | tail -1)"
+
+    if [[ "$(isEmptyString "${unzipFolder}")" = 'true' || "$(wc -l <<< "${unzipFolder}")" != '1' ]]
+    then
+        fatal 'FATAL : multiple unzip folder names found'
+    fi
+
+    if [[ "$(ls -A "${unzipFolder}")" = '' ]]
+    then
+        fatal "FATAL : folder '${unzipFolder}' empty"
+    fi
+
+    # Move Folder
+
+    local currentPath="$(pwd)"
+
+    cd "${unzipFolder}"
+    find '.' -maxdepth 1 ! -name '.' -exec mv '{}' "${awscliInstallFolder}" \;
+    cd "${currentPath}"
+    rm -f -r "${unzipFolder}"
 
     # Config Profile
 
