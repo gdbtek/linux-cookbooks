a41eab01f0b1ef9824bb0b730a1bc1bfe6e1f915
Nam Nguyen <gdbtek@users.noreply.github.com>
2015-03-31 13:52:15 -0700

    Update default.bash

--- cookbooks/nginx/attributes/default.bash
+++ cookbooks/nginx/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export nginxDownloadURL='http://nginx.org/download/nginx-1.7.10.tar.gz'
+export nginxDownloadURL='http://nginx.org/download/nginx-1.7.11.tar.gz'
 export nginxPCREDownloadURL='ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.36.tar.gz'
 export nginxZLIBDownloadURL='http://zlib.net/zlib-1.2.8.tar.gz'
 
