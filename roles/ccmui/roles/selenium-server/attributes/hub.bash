d3e484ac9ae3b8e61a94d94294dabd4fc87d9a4a
Nam Nguyen <gdbtek@yahoo.com>
2015-03-31 17:17:33 -0700

    add hosts

--- roles/ccmui/roles/selenium-server/attributes/hub.bash
+++ roles/ccmui/roles/selenium-server/attributes/hub.bash
@@ -2,3 +2,11 @@
 
 export ccmuiSeleniumServerDisk='/dev/sdb'
 export ccmuiSeleniumServerMountOn='/opt'
+
+export ccmuiSeleniumServerHosts=(
+    '192.168.239.3  selenium-win-2.ccmui.adobe.com'
+    '192.168.239.4  selenium-win-3.ccmui.adobe.com'
+    '192.168.239.19 selenium-win-4.ccmui.adobe.com'
+    '192.168.239.20 selenium-win-5.ccmui.adobe.com'
+    '192.168.239.22 selenium-win-6.ccmui.adobe.com'
+)
--- roles/ccmui/roles/selenium-server/recipes/it-cloud-hub-selenium-server.bash
+++ roles/ccmui/roles/selenium-server/recipes/it-cloud-hub-selenium-server.bash
@@ -24,6 +24,16 @@ function main()
 
     addUserAuthorizedKey "$(whoami)" "$(whoami)" "$(cat "${appPath}/../files/default/authorized_keys")"
 
+    # Config Hosts
+
+    local host=''
+
+    for host in "${ccmuiSeleniumServerHosts[@]}"
+    do
+        header "ADDING HOST '${host}' to '/etc/hosts'"
+        appendToFileIfNotFound '/etc/hosts' "${host}" "${host}" 'false' 'false' 'false'
+    done
+
     # Clean Up
 
     cleanUpSystemFolders
