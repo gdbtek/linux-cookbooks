f4bc50a9af382ceab435f212af0ed43e00d7f9d3
Nam Nguyen <gdbtek@yahoo.com>
2015-04-07 10:56:53 -0700

    update mysql version

--- cookbooks/mysql/attributes/default.bash
+++ cookbooks/mysql/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export mysqlDownloadURL='http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.23-linux-glibc2.5-x86_64.tar.gz'
+export mysqlDownloadURL='http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.24-linux-glibc2.5-x86_64.tar.gz'
 
 export mysqlInstallFolder='/opt/mysql'
 
