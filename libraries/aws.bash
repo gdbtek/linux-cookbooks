9403969cb003ca713f1a2998d86adf87748bc947
Nam Nguyen <gdbtek@yahoo.com>
2015-04-09 12:05:29 -0700

    add AWS util

---
+++ libraries/aws.bash
@@ -0,0 +1,76 @@
+#!/bin/bash -e
+
+source "$(dirname "${BASH_SOURCE[0]}")/util.bash"
+
+#####################
+# GENERAL UTILITIES #
+#####################
+
+function getAllowedRegions()
+{
+    echo 'ap-northeast-1 ap-southeast-1 ap-southeast-2 eu-central-1 eu-west-1 sa-east-1 us-east-1 us-west-1 us-west-2'
+}
+
+function isValidRegion()
+{
+    local -r region="${1:?}"
+
+    local -r allowedRegions=($(getAllowedRegions))
+
+    echo "$(isElementInArray "${region}" "${allowedRegions[@]}")"
+}
+
+#######################
+# META-DATA UTILITIES #
+#######################
+
+function getInstanceAvailabilityZone()
+{
+    echo "$(curl -s --retry 3 --retry-delay 5 'http://instance-data/latest/meta-data/placement/availability-zone')"
+}
+
+function getInstanceID()
+{
+    echo "$(curl -s --retry 3 --retry-delay 5 'http://instance-data/latest/meta-data/instance-id')"
+}
+
+################
+# S3 UTILITIES #
+################
+
+function getS3Protocol()
+{
+    local -r region="${1:?}"
+
+    if [[ "${region}" = 'us-east-1' ]]
+    then
+        echo 's3'
+    elif [[ "$(isValidRegion "${region}")" = 'true' ]]
+    then
+        echo "s3-${region}"
+    else
+        echo
+    fi
+}
+
+function getInstanceS3Protocol()
+{
+    local -r availabilityZone="$(getInstanceAvailabilityZone)"
+
+    echo "$(getS3Protocol "${availabilityZone:0:${#availabilityZone} - 1}")"
+}
+
+#######################
+# USER-DATA UTILITIES #
+#######################
+
+function getUserDataValue()
+{
+    local -r key="${1:?}"
+
+    local -r value="$(curl -s --retry 3 --retry-delay 5 'http://instance-data/latest/user-data' | \
+                      grep -E "^\s*${key:?}\s*=\s*" | \
+                      awk -F '=' '{ print $2 }')"
+
+    echo "$(trimString "${value:?}")"
+}
