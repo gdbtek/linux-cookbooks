22d9a36126984b1b0a6f385961a555f39351d489
Nam Nguyen <gdbtek@yahoo.com>
2015-04-08 19:43:01 -0700

    avoid upgrade prompt

--- roles/ccmui/roles/selenium-server/tools/recipes/upgrade.bash
+++ roles/ccmui/roles/selenium-server/tools/recipes/upgrade.bash
@@ -4,9 +4,11 @@ function main()
 {
     local appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     local command="sudo apt-get update -m &&
-                   sudo apt-get upgrade --force-yes -m -y &&
-                   sudo apt-get dist-upgrade --force-yes -m -y &&
-                   sudo apt-get autoremove -y"
+                   sudo DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade &&
+                   sudo DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade &&
+                   sudo apt-get autoremove -y &&
+                   sudo apt-get clean &&
+                   sudo apt-get autoclean"
 
     "${appPath}/../../../../../../tools/run-remote-command.bash" \
         --attribute-file "${appPath}/../attributes/selenium.bash" \
