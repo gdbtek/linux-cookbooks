2be20548e99fa291a6d61d3de540855cd1e953d2
Nam Nguyen <gdbtek@yahoo.com>
2015-05-29 00:24:17 -0700

    add new server

--- roles/adobe/roles/ccmui/roles/selenium-server/attributes/hub.bash
+++ roles/adobe/roles/ccmui/roles/selenium-server/attributes/hub.bash
@@ -4,6 +4,7 @@ export CCMUI_SELENIUM_SERVER_DISK='/dev/sdb'
 export CCMUI_SELENIUM_SERVER_MOUNT_ON='/opt'
 
 export CCMUI_SELENIUM_SERVER_HOSTS=(
+    '192.168.239.6  selenium-win-1.ccmui.adobe.com'
     '192.168.239.3  selenium-win-2.ccmui.adobe.com'
     '192.168.239.4  selenium-win-3.ccmui.adobe.com'
     '192.168.239.19 selenium-win-4.ccmui.adobe.com'
