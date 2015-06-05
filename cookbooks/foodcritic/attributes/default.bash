9a59b0f0aa88045cbde3bb5ee18ede1d07979e11
Nam Nguyen <gdbtek@yahoo.com>
2015-06-04 22:49:21 -0700

    add foodcritic cookbook

---
+++ cookbooks/foodcritic/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+source "$(dirname "${BASH_SOURCE[0]}")/../../ruby/attributes/default.bash"
+
+export FOODCRITIC_RUBY_INSTALL_FOLDER="${RUBY_INSTALL_FOLDER}"
---
+++ cookbooks/foodcritic/recipes/install.bash
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    if [[ "$(existCommand 'ruby')" = 'false' || ! -d "${FOODCRITIC_RUBY_INSTALL_FOLDER}" ]]
+    then
+        "${appPath}/../../ruby/recipes/install.bash" "${FOODCRITIC_RUBY_INSTALL_FOLDER}"
+    fi
+}
+
+function install()
+{
+    # Install
+
+    gem install foodcritic
+
+    if [[ -f "${FOODCRITIC_RUBY_INSTALL_FOLDER}/bin/foodcritic" ]]
+    then
+        ln -f -s "${FOODCRITIC_RUBY_INSTALL_FOLDER}/bin/foodcritic" '/usr/local/bin/foodcritic'
+    fi
+
+    # Display Version
+
+    info "\n$(foodcritic --version)"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appPath}/../../../libraries/util.bash"
+    source "${appPath}/../attributes/default.bash"
+
+    checkRequireSystem
+    checkRequireRootUser
+
+    header 'INSTALLING FOODCRITIC'
+
+    # Install
+
+    installDependencies
+    install
+    installCleanUp
+}
+
+main "${@}"
