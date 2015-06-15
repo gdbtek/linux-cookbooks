ecf4eac4a929454a11686a4a95477c0f23c43fcf
Nam Nguyen <gdbtek@yahoo.com>
2015-06-14 22:45:40 -0700

    resolve spellcheck errors

--- roles/adobe/roles/ccmui/roles/aws/tools/recipes/dns-resolver.bash
+++ roles/adobe/roles/ccmui/roles/aws/tools/recipes/dns-resolver.bash
@@ -5,8 +5,8 @@ function main()
     local -r attributeFile="${1}"
 
     local -r appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    local -r command='grep -o "^CCMUI_DNS_RESOLVER_IP=.*$" "/apps/scripts/ccweb.sh" &&
-                      grep -F 'nameserver' "/etc/resolv.conf"'
+    local -r command="grep -o '^CCMUI_DNS_RESOLVER_IP=.*$' '/apps/scripts/ccweb.sh' &&
+                      grep -F 'nameserver' '/etc/resolv.conf'"
 
     "${appPath}/../../../../../../../../tools/run-remote-command.bash" \
         --attribute-file "${attributeFile}" \
