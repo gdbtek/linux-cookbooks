8ac798bb412fddd51fb99c71a8ea05c742018232
Nam Nguyen <gdbtek@yahoo.com>
2015-06-17 15:20:13 -0700

    update version

--- cookbooks/mongodb/attributes/default.bash
+++ cookbooks/mongodb/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export MONGODB_DOWNLOAD_URL='https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.3.tgz'
+export MONGODB_DOWNLOAD_URL='https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.4.tgz'
 
 export MONGODB_INSTALL_FOLDER='/opt/mongodb'
 export MONGODB_INSTALL_DATA_FOLDER='/opt/mongodb/data'
