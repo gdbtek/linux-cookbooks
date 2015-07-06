301dcf422212a89767452c738415f44487e508b4
Nam Nguyen <gdbtek@yahoo.com>
2015-07-05 23:53:35 -0700

    update version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://dl.bintray.com/mitchellh/packer/packer_0.8.0_linux_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://dl.bintray.com/mitchellh/packer/packer_0.8.1_linux_amd64.zip'
 
 export PACKER_INSTALL_FOLDER='/opt/packer'
