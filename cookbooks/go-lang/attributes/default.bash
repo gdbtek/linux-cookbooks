6934f8e51538379f0073917b50b9687f7a9b1b7f
Nam Nguyen <gdbtek@yahoo.com>
2015-08-21 14:40:43 -0700

    update versions

--- cookbooks/go-lang/attributes/default.bash
+++ cookbooks/go-lang/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz'
+export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz'
 
 export GO_LANG_INSTALL_FOLDER='/opt/go-lang'
