2013e16095f47b2d1a6408ade4f9b8581a8bd324
Nam Nguyen <gdbtek@yahoo.com>
2015-08-26 02:31:47 -0700

    add install server and agent

---
+++ cookbooks/go-cd/recipes/install-server-and-agent.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+function install()
+{
+    "${appPath}/install-server.bash"
+    "${appPath}/install-agent.bash"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appPath}/../../../libraries/util.bash"
+
+    checkRequireSystem
+    checkRequireRootUser
+
+    install
+    installCleanUp
+}
+
+main "${@}"
