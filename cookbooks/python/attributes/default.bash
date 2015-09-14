f3d1ae98ec68497394f876e93c604367af13d066
Nam Nguyen <gdbtek@yahoo.com>
2015-09-14 01:06:04 -0700

    update Python version

--- cookbooks/python/attributes/default.bash
+++ cookbooks/python/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PYTHON_DOWNLOAD_URL='https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz'
+export PYTHON_DOWNLOAD_URL='https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz'
 
 export PYTHON_INSTALL_FOLDER='/opt/python'
