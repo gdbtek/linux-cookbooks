ca7a095006517970cb68f33de0edd9d770c5c3d9
Nam Nguyen <gdbtek@yahoo.com>
2015-09-22 20:55:51 -0700

    update version

--- cookbooks/elastic-search/attributes/default.bash
+++ cookbooks/elastic-search/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export ELASTIC_SEARCH_DOWNLOAD_URL='https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz'
+export ELASTIC_SEARCH_DOWNLOAD_URL='https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz'
 
 export ELASTIC_SEARCH_INSTALL_FOLDER='/opt/elastic-search'
 export ELASTIC_SEARCH_JDK_INSTALL_FOLDER="${JDK_INSTALL_FOLDER}"
