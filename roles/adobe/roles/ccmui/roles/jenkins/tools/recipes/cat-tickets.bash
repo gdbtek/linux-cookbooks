3bd99af19cb11796b6a4e4f46475e3c797486dc6
Nam Nguyen <gdbtek@yahoo.com>
2015-09-23 15:17:31 -0700

    update

--- roles/adobe/roles/ccmui/roles/jenkins/tools/recipes/cat-tickets.bash
+++ roles/adobe/roles/ccmui/roles/jenkins/tools/recipes/cat-tickets.bash
@@ -6,7 +6,7 @@ function main()
 
     local -r appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-    local -r command="find '/Users' -type f -name '*ticket*' -exec ls -la {} \; -exec cat {} \;"
+    local -r command="find '/Users' -type f -name '*ticket*' -exec printf '\n' \; -exec ls -la {} \; -exec cat {} \; -exec printf '\n' \;"
 
     "${appPath}/../../../../../../../../tools/run-remote-command.bash" \
         --attribute-file "${attributeFile}" \
