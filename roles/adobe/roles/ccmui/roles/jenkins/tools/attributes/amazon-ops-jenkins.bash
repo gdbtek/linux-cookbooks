107cbc7039d910544d5c951a1c5bbc657faa6cb6
Nam Nguyen <gdbtek@yahoo.com>
2015-09-25 10:22:27 -0700

    update files names

---
+++ roles/adobe/roles/ccmui/roles/jenkins/tools/attributes/amazon-ops-jenkins.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+export SSH_LOGIN='ubuntu'
+export SSH_IDENTITY_FILE='/Volumes/Data/Data/Keys/ssh/adobe/aws/ccmui/development.pem'
+
+export MASTER_SERVERS=(
+    'jenkins-pub.ccmui.adobe.com'
+)
+
+export SLAVE_SERVERS=(
+
+)
