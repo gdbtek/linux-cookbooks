5ab8fc01f8ad4b985585465d32dbbc6421040fb4
Nam Nguyen <gdbtek@yahoo.com>
2015-09-29 09:54:07 -0700

    add tools

---
+++ roles/adobe/roles/ccmui/roles/jenkins/tools/recipes/get-ip.bash
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+function main()
+{
+    local -r attributeFile="${1}"
+
+    local -r appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    local -r command='ifconfig | grep -E -i "inet addr.*Bcast.*Mask" | cut -d ":" -f2 | cut -d " " -f1'
+
+    "${appPath}/../../../../../../../../tools/run-remote-command.bash" \
+        --attribute-file "${attributeFile}" \
+        --command "${command}" \
+        --machine-type 'masters-slaves'
+}
+
+main "${@}"
