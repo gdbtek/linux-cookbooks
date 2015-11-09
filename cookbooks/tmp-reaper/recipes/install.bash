4c628998a08898cb1fa169677a1a8697f65f3621
Nam Nguyen <gdbtek@yahoo.com>
2015-11-09 15:47:31 -0800

    update

--- cookbooks/tmp-reaper/recipes/install.bash
+++ cookbooks/tmp-reaper/recipes/install.bash
@@ -13,7 +13,7 @@ function install()
 
     for ((i = 0; i < ${#TMP_REAPER_FOLDERS[@]}; i = i + 3))
     do
-        content="${content}\ntmpreaper -a -f -s -v --protect '${TMP_REAPER_FOLDERS[${i} + 1]}' '${TMP_REAPER_FOLDERS[${i} + 2]}' '${TMP_REAPER_FOLDERS[${i}]}'"
+        content="tmpreaper -a -f -s -v --protect '${TMP_REAPER_FOLDERS[${i} + 1]}' '${TMP_REAPER_FOLDERS[${i} + 2]}' '${TMP_REAPER_FOLDERS[${i}]}'\n${content}"
     done
 
     echo -e "${content}" > "${TMP_REAPER_CRON_FOLDER}/tmp-reaper"
