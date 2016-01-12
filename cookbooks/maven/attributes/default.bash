8efe59e1d17687935bee614064925be45d348b0f
Nam Nguyen <gdbtek@yahoo.com>
2016-01-12 00:12:27 -0800

    update version

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz'
+export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz'
 
 export MAVEN_INSTALL_FOLDER='/opt/maven'
 export MAVEN_JDK_INSTALL_FOLDER="${JDK_INSTALL_FOLDER}"
