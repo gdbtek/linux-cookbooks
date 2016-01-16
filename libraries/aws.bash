c71bf69e745d42edc9b21d5c070e9dbb252d5a2c
Nam Nguyen <gdbtek@yahoo.com>
2016-01-15 21:49:35 -0800

    update region

--- libraries/aws.bash
+++ libraries/aws.bash
@@ -45,7 +45,7 @@ function updateInstanceName()
 
 function getAllowedRegions()
 {
-    echo 'ap-northeast-1 ap-southeast-1 ap-southeast-2 eu-central-1 eu-west-1 sa-east-1 us-east-1 us-west-1 us-west-2'
+    echo 'ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 eu-central-1 eu-west-1 sa-east-1 us-east-1 us-west-1 us-west-2'
 }
 
 function isValidRegion()
