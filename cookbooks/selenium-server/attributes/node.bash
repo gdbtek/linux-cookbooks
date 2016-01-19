36763140c0d1adf78f47dda9b293265136d34188
Nam Nguyen <gdbtek@yahoo.com>
2016-01-19 12:01:59 -0800

    update selenium driver version

--- cookbooks/selenium-server/attributes/node.bash
+++ cookbooks/selenium-server/attributes/node.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar'
+export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.0.jar'
 
 export SELENIUM_SERVER_INSTALL_FOLDER='/opt/selenium-server/node'
 export SELENIUM_SERVER_JDK_INSTALL_FOLDER="${JDK_INSTALL_FOLDER}"
