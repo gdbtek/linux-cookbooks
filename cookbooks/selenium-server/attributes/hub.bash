6b443649f75c50868b0e161dcd4be2a212c716ec
Nam Nguyen <gdbtek@yahoo.com>
2016-01-27 22:25:49 -0800

    update version

--- cookbooks/selenium-server/attributes/hub.bash
+++ cookbooks/selenium-server/attributes/hub.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.0.jar'
+export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/2.50/selenium-server-standalone-2.50.0.jar'
 
 export SELENIUM_SERVER_INSTALL_FOLDER='/opt/selenium-server/hub'
 export SELENIUM_SERVER_JDK_INSTALL_FOLDER="${JDK_INSTALL_FOLDER}"
