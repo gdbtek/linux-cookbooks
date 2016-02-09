f9ea9cb21c76f6946d8873e65f896c78936cbe28
Nam Nguyen <gdbtek@yahoo.com>
2016-02-09 10:53:43 -0800

    update version

--- cookbooks/node-js/attributes/default.bash
+++ cookbooks/node-js/attributes/default.bash
@@ -2,7 +2,7 @@
 
 export NODE_JS_INSTALL_FOLDER='/opt/node-js'
 
-# export NODE_JS_VERSION='v0.10.41'
+# export NODE_JS_VERSION='v0.10.42'
 export NODE_JS_VERSION='latest'
 
 export NODE_JS_INSTALL_NPM_PACKAGES=(
