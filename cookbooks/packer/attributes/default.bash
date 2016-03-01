2318936a0fbf9375100dc7b805af414aae96fd53
Nam Nguyen <gdbtek@yahoo.com>
2016-03-01 10:26:35 -0800

    update packer version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_amd64.zip'
 
 export PACKER_INSTALL_FOLDER='/opt/packer'
