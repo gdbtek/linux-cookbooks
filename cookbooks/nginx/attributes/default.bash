9e16878f9c94190604705156be8ffd1b89a888e7
Nam Nguyen <gdbtek@yahoo.com>
2016-03-04 03:14:33 -0800

    update version

--- cookbooks/nginx/attributes/default.bash
+++ cookbooks/nginx/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export NGINX_DOWNLOAD_URL='http://nginx.org/download/nginx-1.9.11.tar.gz'
+export NGINX_DOWNLOAD_URL='http://nginx.org/download/nginx-1.9.12.tar.gz'
 export NGINX_PCRE_DOWNLOAD_URL='ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz'
 export NGINX_ZLIB_DOWNLOAD_URL='http://zlib.net/zlib-1.2.8.tar.gz'
 
