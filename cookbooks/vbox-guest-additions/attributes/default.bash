23d73be6edda85659bb8f63dd520554ad385e68a
Nam Nguyen <gdbtek@yahoo.com>
2016-03-06 20:58:06 -0800

    update virtual-box version

--- cookbooks/vbox-guest-additions/attributes/default.bash
+++ cookbooks/vbox-guest-additions/attributes/default.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export VBOX_GUEST_ADDITIONS_DOWNLOAD_URL='http://download.virtualbox.org/virtualbox/5.0.14/VBoxGuestAdditions_5.0.14.iso'
+export VBOX_GUEST_ADDITIONS_DOWNLOAD_URL='http://download.virtualbox.org/virtualbox/5.0.16/VBoxGuestAdditions_5.0.16.iso'
