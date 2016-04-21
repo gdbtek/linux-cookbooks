ce42fdb31120d1837e74c139fdebfda78220fea4
Nam Nguyen <gdbtek@yahoo.com>
2016-04-21 12:11:34 -0700

    add wild-fly cookbook

---
+++ cookbooks/wild-fly/attributes/default.bash
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
+
+export WILD_FLY_JDK_INSTALL_FOLDER="${JDK_INSTALL_FOLDER}"
+
+export WILD_FLY_DOWNLOAD_URL='http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.tar.gz'
+export WILD_FLY_INSTALL_FOLDER='/opt/wild-fly'
+
+export WILD_FLY_USER_NAME='wild-fly'
+export WILD_FLY_GROUP_NAME='wild-fly'
+
+export WILD_FLY_MANAGEMENT_USER='wild-fly'
+export WILD_FLY_MANAGEMENT_PASSWORD='wild-fly'
+
+export WILD_FLY_SERVICE_NAME='wild-fly'
+
+export WILD_FLY_APPLICATION_BIND_ADDRESS='0.0.0.0'
+export WILD_FLY_MANAGEMENT_BIND_ADDRESS='localhost'
---
+++ cookbooks/wild-fly/recipes/install.bash
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    if [[ "$(existCommand 'java')" = 'false' || ! -d "${WILD_FLY_JDK_INSTALL_FOLDER}" ]]
+    then
+        "${APP_FOLDER_PATH}/../../jdk/recipes/install.bash" "${WILD_FLY_JDK_INSTALL_FOLDER}"
+    fi
+}
+
+function install()
+{
+    # Clean Up
+
+    initializeFolder "${WILD_FLY_INSTALL_FOLDER}"
+
+    # Install
+
+    unzipRemoteFile "${WILD_FLY_DOWNLOAD_URL}" "${WILD_FLY_INSTALL_FOLDER}"
+
+    # Config Upstart
+
+    local -r upstartConfigData=(
+        '__APPLICATION_BIND_ADDRESS__' "${WILD_FLY_APPLICATION_BIND_ADDRESS}"
+        '__GROUP_NAME__' "${WILD_FLY_GROUP_NAME}"
+        '__INSTALL_FOLDER__' "${WILD_FLY_INSTALL_FOLDER}"
+        '__MANAGEMENT_BIND_ADDRESS__' "${WILD_FLY_MANAGEMENT_BIND_ADDRESS}"
+        '__USER_NAME__' "${WILD_FLY_USER_NAME}"
+    )
+
+    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/wild-fly.conf.upstart" "/etc/init/${WILD_FLY_SERVICE_NAME}.conf" "${upstartConfigData[@]}"
+
+    # Add Management User
+
+    "${WILD_FLY_INSTALL_FOLDER}/bin/add-user.sh" --user "${WILD_FLY_MANAGEMENT_USER}" --password "${WILD_FLY_MANAGEMENT_PASSWORD}"
+
+    # Start
+
+    addUser "${WILD_FLY_USER_NAME}" "${WILD_FLY_GROUP_NAME}" 'false' 'true' 'false'
+    chown -R "${WILD_FLY_USER_NAME}:${WILD_FLY_GROUP_NAME}" "${WILD_FLY_INSTALL_FOLDER}"
+    start "${WILD_FLY_SERVICE_NAME}"
+
+    # Display Open Ports
+
+    displayOpenPorts '5'
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+    source "${APP_FOLDER_PATH}/../attributes/default.bash"
+
+    checkRequireSystem
+    checkRequireRootUser
+
+    header 'INSTALLING WILD-FLY'
+
+    checkRequirePort '8080' '9990'
+
+    installDependencies
+    install
+    installCleanUp
+}
+
+main "${@}"
