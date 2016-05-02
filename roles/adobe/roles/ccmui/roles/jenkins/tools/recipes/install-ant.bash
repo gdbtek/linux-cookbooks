ee8d4adf7e5dc7be54b34630a253ecafb5057389
Nam Nguyen <gdbtek@yahoo.com>
2016-05-02 01:07:00 -0700

    add ant

---
+++ roles/adobe/roles/ccmui/roles/jenkins/tools/recipes/install-ant.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+function main()
+{
+    local -r attributeFile="${1}"
+
+    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    local -r command="cd /var/tmp &&
+                      sudo rm -f -r ubuntu-cookbooks &&
+                      sudo git clone --depth=1 https://github.com/gdbtek/ubuntu-cookbooks.git &&
+                      sudo /var/tmp/ubuntu-cookbooks/cookbooks/ant/recipes/install.bash
+                      sudo rm -f -r /var/tmp/ubuntu-cookbooks"
+
+    "${appFolderPath}/../../../../../../../../tools/run-remote-command.bash" \
+        --attribute-file "${attributeFile}" \
+        --command "${command}" \
+        --machine-type 'masters-slaves'
+}
+
+main "${@}"
