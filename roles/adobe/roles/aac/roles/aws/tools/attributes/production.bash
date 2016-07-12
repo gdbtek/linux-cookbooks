d1b8d357b8701bcdea24eea23917b6ce313ef0d4
Nam Nguyen <gdbtek@yahoo.com>
2016-07-12 14:02:19 -0700

    update

--- roles/adobe/roles/aac/roles/aws/tools/attributes/production.bash
+++ roles/adobe/roles/aac/roles/aws/tools/attributes/production.bash
@@ -12,15 +12,15 @@ export MASTER_SERVERS=(
 
     # eu-west-1
 
-    'ec2-52-17-188-101.eu-west-1.compute.amazonaws.com'
-    'ec2-52-17-128-219.eu-west-1.compute.amazonaws.com'
-    'ec2-52-16-228-89.eu-west-1.compute.amazonaws.com'
+    'ec2-52-19-213-63.eu-west-1.compute.amazonaws.com'
+    'ec2-52-51-18-254.eu-west-1.compute.amazonaws.com'
+    'ec2-52-51-170-9.eu-west-1.compute.amazonaws.com'
 
     # us-east-1
 
-    'ec2-52-0-58-187.compute-1.amazonaws.com'
     'ec2-52-6-155-176.compute-1.amazonaws.com'
-    'ec2-54-174-157-108.compute-1.amazonaws.com'
+    'ec2-52-0-58-187.compute-1.amazonaws.com'
+    'ec2-54-237-231-97.compute-1.amazonaws.com'
 )
 
 export SLAVE_SERVERS=()
--- roles/adobe/roles/aac/roles/aws/tools/attributes/stage.bash
+++ roles/adobe/roles/aac/roles/aws/tools/attributes/stage.bash
@@ -6,18 +6,18 @@ export SSH_IDENTITY_FILE='/Volumes/Data/Data/Keys/ssh/adobe/aws/aac/stage.pem'
 export MASTER_SERVERS=(
     # ap-northeast-1
 
-    'ec2-52-68-64-45.ap-northeast-1.compute.amazonaws.com'
-    'ec2-52-68-28-125.ap-northeast-1.compute.amazonaws.com'
+    'ec2-54-238-191-218.ap-northeast-1.compute.amazonaws.com'
+    'ec2-54-199-183-153.ap-northeast-1.compute.amazonaws.com'
 
     # eu-west-1
 
-    'ec2-52-17-231-14.eu-west-1.compute.amazonaws.com'
     'ec2-52-16-48-161.eu-west-1.compute.amazonaws.com'
+    'ec2-52-17-231-14.eu-west-1.compute.amazonaws.com'
 
     # us-east-1
 
-    'ec2-52-2-113-142.compute-1.amazonaws.com'
-    'ec2-52-1-53-107.compute-1.amazonaws.com'
+    'ec2-52-87-190-197.compute-1.amazonaws.com'
+    'ec2-54-164-40-52.compute-1.amazonaws.com'
 )
 
 export SLAVE_SERVERS=()
