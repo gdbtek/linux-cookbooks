90ba6354f7201cb42740fc7f6c1371624f81f73e
Nam Nguyen <gdbtek@yahoo.com>
2016-07-12 13:55:11 -0700

    update

---
+++ roles/adobe/roles/aac/roles/aws/tools/recipes/deploy-splunk.bash
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+function main()
+{
+    local -r attributeFile="${1}"
+
+    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    local -r command='sudo chef-client -o "recipe[co-splunk]" && sudo service splunk restart'
+
+    "${appFolderPath}/../../../../../../../../tools/run-remote-command.bash" \
+        --attribute-file "${attributeFile}" \
+        --command "${command}" \
+        --machine-type 'masters'
+}
+
+main "${@}"
