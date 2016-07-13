973929b3b6581e543a12e3132a77125da09b1306
Nam Nguyen <gdbtek@yahoo.com>
2016-07-13 12:08:47 -0700

    clean up

--- roles/adobe/roles/ccmui/roles/selenium-server/attributes/node.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-export CCMUI_SELENIUM_SERVER_DISK='/dev/sdb'
-export CCMUI_SELENIUM_SERVER_MOUNT_ON='/opt'
-
-export CCMUI_SELENIUM_SERVER_HUB_HOST='selenium-private.ccmui.adobe.com'
