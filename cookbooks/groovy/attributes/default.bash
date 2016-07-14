6b80215f441631044ffc9d988dbe12ba4e176038
Nam Nguyen <gdbtek@yahoo.com>
2016-07-14 16:18:57 -0700

    update

--- cookbooks/groovy/attributes/default.bash
+++ cookbooks/groovy/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export GROOVY_DOWNLOAD_URL='http://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.6.zip'
+export GROOVY_DOWNLOAD_URL='https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.7.zip'
 
 export GROOVY_INSTALL_FOLDER='/opt/groovy'
 export GROOVY_JDK_INSTALL_FOLDER="${JDK_INSTALL_FOLDER}"
