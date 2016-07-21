f58bdea1c9b6af586b9180868a2ebd479e7f013a
Nam Nguyen <gdbtek@yahoo.com>
2016-07-21 00:19:54 -0700

    update JDK version

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz'
+export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER='/opt/jdk'
