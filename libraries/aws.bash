3e1cacd4d1d52efad8b9f349e03aea49972888ce
gdbtek <gdbtek@yahoo.com>
2016-10-20 23:32:09 -0700

    bug fixes

--- libraries/aws.bash
+++ libraries/aws.bash
@@ -91,7 +91,7 @@ function updateInstanceName()
 
 function getAllowedRegions()
 {
-    echo 'ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 cn-north-1 eu-central-1 eu-west-1 sa-east-1 us-east-1 us-gov-west-1 us-west-1 us-west-2'
+    echo 'ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 cn-north-1 eu-central-1 eu-west-1 sa-east-1 us-east-1 us-east-2 us-gov-west-1 us-west-1 us-west-2'
 }
 
 function getRegionFromRecordSetAliasTargetDNSName()
