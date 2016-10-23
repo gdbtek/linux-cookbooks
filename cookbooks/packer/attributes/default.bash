e128cb37787ec802baeb3f870257f82b18b6ef1d
gdbtek <gdbtek@yahoo.com>
2016-10-22 22:27:41 -0700

    update packer version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/0.10.2/packer_0.10.2_linux_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip'
 
 export PACKER_INSTALL_FOLDER='/opt/packer'
