8a8a1e22c4e41bc3374a59bd0a76d46fe1f01910
gdbtek <gdbtek@yahoo.com>
2016-11-02 00:27:16 -0700

    add parallel cookbook

---
+++ cookbooks/parallel/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+export PARALLEL_DOWNLOAD_URL='ftp://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2'
+
+export PARALLEL_INSTALL_FOLDER='/opt/parallel'
---
+++ cookbooks/parallel/recipes/install.bash
@@ -0,0 +1,69 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    installBuildEssential
+}
+
+function install()
+{
+    umask '0022'
+
+    # Clean Up
+
+    initializeFolder "${PARALLEL_INSTALL_FOLDER}"
+
+    # Install
+
+    local -r tempFolder="$(getTemporaryFolder)"
+
+    unzipRemoteFile "${PARALLEL_DOWNLOAD_URL}" "${tempFolder}"
+    cd "${tempFolder}"
+    "${tempFolder}/configure"
+    make
+    make install
+    symlinkLocalBin "${PARALLEL_INSTALL_FOLDER}/bin"
+    rm -f -r "${tempFolder}"
+
+    # Config Profile
+
+    local -r profileConfigData=('__INSTALL_FOLDER__' "${PARALLEL_INSTALL_FOLDER}")
+
+    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/parallel.sh.profile" '/etc/profile.d/parallel.sh' "${profileConfigData[@]}"
+
+    # Display Version
+
+    displayVersion "$(parallel --version)"
+
+    umask '0077'
+}
+
+function main()
+{
+    local -r installFolder="${1}"
+
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+    source "${APP_FOLDER_PATH}/../attributes/default.bash"
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    header 'INSTALLING PARALLEL'
+
+    # Override Default Config
+
+    if [[ "$(isEmptyString "${installFolder}")" = 'false' ]]
+    then
+        PARALLEL_INSTALL_FOLDER="${installFolder}"
+    fi
+
+    # Install
+
+    installDependencies
+    install
+    installCleanUp
+}
+
+main "${@}"
