162057aebbc173432825fb140864ad259bfeb506
gdbtek <gdbtek@yahoo.com>
2016-12-08 12:01:35 -0800

    support redhat and centos for nginx

--- cookbooks/nginx/recipes/install-from-binary.bash
+++ cookbooks/nginx/recipes/install-from-binary.bash
@@ -4,19 +4,38 @@ function install()
 {
     umask '0022'
 
-    # Update Apt Source List
+    # Add Package Link
 
-    local -r releaseFilePath='/etc/lsb-release'
+    if [[ "$(isUbuntuDistributor)" = 'true' ]]
+    then
+        local -r releaseFilePath='/etc/lsb-release'
 
-    checkExistFile "${releaseFilePath}"
+        checkExistFile "${releaseFilePath}"
 
-    source "${releaseFilePath}"
+        source "${releaseFilePath}"
 
-    local -r configData=('__DISTRIBUTION_CODE_NAME__' "${DISTRIB_CODENAME}")
+        local -r configData=('__DISTRIBUTION_CODE_NAME__' "${DISTRIB_CODENAME}")
 
-    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/nginx.list.apt" '/etc/apt/sources.list.d/nginx.list' "${configData[@]}"
-    curl -s -L 'http://nginx.org/keys/nginx_signing.key' --retry 12 --retry-delay 5 | apt-key add -
-    apt-get update -m
+        createFileFromTemplate "${APP_FOLDER_PATH}/../templates/nginx.list.apt" '/etc/apt/sources.list.d/nginx.list' "${configData[@]}"
+        curl -s -L 'http://nginx.org/keys/nginx_signing.key' --retry 12 --retry-delay 5 | apt-key add -
+        apt-get update -m
+    elif [[ "$(isCentOSDistributor)" = 'true' || "$(isRedHatDistributor)" = 'true' ]]
+    then
+        local -r releaseFilePath='/etc/os-release'
+
+        checkExistFile "${releaseFilePath}"
+
+        source "${releaseFilePath}"
+
+        local -r configData=(
+            '__PLATFORM_FAMILY__' "${ID}"
+            '__PLATFORM_VERSION__' "$(awk -F '.' '{ print $1 }' <<< "${VERSION_ID}")"
+        )
+
+        createFileFromTemplate "${APP_FOLDER_PATH}/../templates/nginx.repo" '/etc/yum.repos.d/nginx.repo' "${configData[@]}"
+    else
+        fatal '\nFATAL : only support CentOS, RedHat or Ubuntu OS'
+    fi
 
     # Install
 
