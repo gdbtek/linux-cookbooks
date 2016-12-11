9edadd76d2dfabc395a4f87c25f97cef8e1afc4a
gdbtek <gdbtek@yahoo.com>
2016-12-10 22:09:55 -0800

    update version

--- cookbooks/go-lang/attributes/default.bash
+++ cookbooks/go-lang/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz'
+export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz'
 
 export GO_LANG_INSTALL_FOLDER='/opt/go-lang'
