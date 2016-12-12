55767e0d45c1bf57d50d7a01937b530537b7180d
gdbtek <gdbtek@yahoo.com>
2016-12-12 10:06:41 -0800

    update version

--- cookbooks/go-cd/attributes/default.bash
+++ cookbooks/go-cd/attributes/default.bash
@@ -2,8 +2,8 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export GO_CD_SERVER_DOWNLOAD_URL='https://download.go.cd/binaries/16.11.0-4185/generic/go-server-16.11.0-4185.zip'
-export GO_CD_AGENT_DOWNLOAD_URL='https://download.go.cd/binaries/16.11.0-4185/generic/go-agent-16.11.0-4185.zip'
+export GO_CD_SERVER_DOWNLOAD_URL='https://download.go.cd/binaries/16.12.0-4352/generic/go-server-16.12.0-4352.zip'
+export GO_CD_AGENT_DOWNLOAD_URL='https://download.go.cd/binaries/16.12.0-4352/generic/go-agent-16.12.0-4352.zip'
 
 export GO_CD_SERVER_INSTALL_FOLDER='/opt/go-cd/server'
 export GO_CD_AGENT_INSTALL_FOLDER='/opt/go-cd/agents/agent'
--- cookbooks/mysql/attributes/default.bash
+++ cookbooks/mysql/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export MYSQL_DOWNLOAD_URL='http://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.16-linux-glibc2.5-x86_64.tar.gz'
+export MYSQL_DOWNLOAD_URL='http://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.17-linux-glibc2.5-x86_64.tar.gz'
 
 export MYSQL_INSTALL_FOLDER='/opt/mysql'
 
