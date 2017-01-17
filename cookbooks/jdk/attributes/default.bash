ae882acd7c1313eb88b50ccacef8264de7eaf397
gdbtek <gdbtek@yahoo.com>
2017-01-17 15:44:21 -0800

    update jdk temporary

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u121-b13/jdk-8u121-linux-x64.tar.gz'
+export JDK_DOWNLOAD_URL='https://dl.dropboxusercontent.com/u/15529567/downloads/linux/jdk-8-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER='/opt/jdk'
