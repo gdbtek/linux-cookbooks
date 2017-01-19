3e4fc039ebd4bdab4b9b3c978982ccc4d8a9ff61
gdbtek <gdbtek@yahoo.com>
2017-01-18 21:46:31 -0800

    update url for JDK

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='https://dl.dropboxusercontent.com/u/15529567/downloads/linux/jdk-8-linux-x64.tar.gz'
+export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER='/opt/jdk'
