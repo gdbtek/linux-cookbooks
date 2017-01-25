2d05a05b883cfc41716eb95749f1dae168cc3f67
gdbtek <gdbtek@yahoo.com>
2017-01-25 01:48:47 -0800

    update nginx version

--- cookbooks/nginx/attributes/default.bash
+++ cookbooks/nginx/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export NGINX_DOWNLOAD_URL='http://nginx.org/download/nginx-1.11.8.tar.gz'
+export NGINX_DOWNLOAD_URL='http://nginx.org/download/nginx-1.11.9.tar.gz'
 export NGINX_PCRE_DOWNLOAD_URL='http://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.40.tar.gz'
 export NGINX_ZLIB_DOWNLOAD_URL='http://zlib.net/zlib-1.2.11.tar.gz'
 
