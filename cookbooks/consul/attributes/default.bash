42c3b3b51388c819364b9f907f54d4ae174067f6
gdbtek <gdbtek@yahoo.com>
2017-01-26 12:17:46 -0800

    update version

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/0.7.2/consul_0.7.2_linux_amd64.zip'
+export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/0.7.3/consul_0.7.3_linux_amd64.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/opt/consul'
--- cookbooks/terraform/attributes/default.bash
+++ cookbooks/terraform/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.8.4/terraform_0.8.4_linux_amd64.zip'
+export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.8.5/terraform_0.8.5_linux_amd64.zip'
 
 export TERRAFORM_INSTALL_FOLDER_PATH='/opt/terraform'
