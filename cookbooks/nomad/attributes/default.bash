0283065e47c108d7642aebffa7dc8d33ffd642d7
gdbtek <gdbtek@yahoo.com>
2017-01-31 00:35:00 -0800

    update nomad version

--- cookbooks/nomad/attributes/default.bash
+++ cookbooks/nomad/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.5.2/nomad_0.5.2_linux_amd64.zip'
+export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.5.3/nomad_0.5.3_linux_amd64.zip'
 
 export NOMAD_INSTALL_FOLDER_PATH='/opt/nomad'
