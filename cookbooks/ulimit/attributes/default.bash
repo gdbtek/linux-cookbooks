d2632f69524f99334741770e159d51874dd86779
gdbtek <gdbtek@yahoo.com>
2017-01-31 11:33:31 -0800

    add ulimit

---
+++ cookbooks/ulimit/attributes/default.bash
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+export ULIMIT_CONFIG_FILE_PATH='/etc/security/limits.conf'
---
+++ cookbooks/ulimit/recipes/install.bash
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+function install()
+{
+    umask '0022'
+
+    cp -f "${APP_FOLDER_PATH}/../files/limits.conf" "${ULIMIT_CONFIG_FILE_PATH}"
+    displayVersion "$(cat "${ULIMIT_CONFIG_FILE_PATH}")"
+
+    umask '0077'
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+    source "${APP_FOLDER_PATH}/../attributes/default.bash"
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    header 'INSTALLING ULIMIT'
+
+    install
+    installCleanUp
+}
+
+main "${@}"
