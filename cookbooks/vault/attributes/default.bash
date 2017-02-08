1ab4112c7ab617c4878adacb0533b6d92a31bc04
gdbtek <gdbtek@yahoo.com>
2017-02-08 02:18:24 -0800

    update version

--- cookbooks/vault/attributes/default.bash
+++ cookbooks/vault/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.6.4/vault_0.6.4_linux_amd64.zip'
+export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.6.5/vault_0.6.5_linux_amd64.zip'
 
 export VAULT_INSTALL_FOLDER_PATH='/opt/vault'
