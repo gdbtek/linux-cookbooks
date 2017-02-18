3812f72f155f8fe35467eaa20b5c7d31fca63638
gdbtek <gdbtek@yahoo.com>
2017-02-18 00:32:12 -0800

    update version

--- cookbooks/ant/attributes/default.bash
+++ cookbooks/ant/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export ANT_DOWNLOAD_URL='http://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.0-bin.tar.gz'
+export ANT_DOWNLOAD_URL='http://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.1-bin.tar.gz'
 
 export ANT_INSTALL_FOLDER_PATH='/opt/ant'
 export ANT_JDK_INSTALL_FOLDER_PATH="${JDK_INSTALL_FOLDER_PATH}"
--- cookbooks/terraform/attributes/default.bash
+++ cookbooks/terraform/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.8.6/terraform_0.8.6_linux_amd64.zip'
+export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.8.7/terraform_0.8.7_linux_amd64.zip'
 
 export TERRAFORM_INSTALL_FOLDER_PATH='/opt/terraform'
