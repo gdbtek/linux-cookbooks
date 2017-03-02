2037d0b495e85158e997a485addcc0a5a308d013
gdbtek <gdbtek@yahoo.com>
2017-03-02 00:37:49 -0800

    update version

--- cookbooks/go-cd/attributes/default.bash
+++ cookbooks/go-cd/attributes/default.bash
@@ -2,8 +2,8 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export GO_CD_SERVER_DOWNLOAD_URL='https://download.gocd.io/binaries/17.1.0-4511/generic/go-server-17.1.0-4511.zip'
-export GO_CD_AGENT_DOWNLOAD_URL='https://download.gocd.io/binaries/17.1.0-4511/generic/go-agent-17.1.0-4511.zip'
+export GO_CD_SERVER_DOWNLOAD_URL='https://download.gocd.io/binaries/17.2.0-4587/generic/go-server-17.2.0-4587.zip'
+export GO_CD_AGENT_DOWNLOAD_URL='https://download.gocd.io/binaries/17.2.0-4587/generic/go-agent-17.2.0-4587.zip'
 
 export GO_CD_SERVER_INSTALL_FOLDER_PATH='/opt/go-cd/server'
 export GO_CD_AGENT_INSTALL_FOLDER_PATH='/opt/go-cd/agents/agent'
--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/0.12.2/packer_0.12.2_linux_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip'
 
 export PACKER_INSTALL_FOLDER_PATH='/opt/packer'
