a9fb12af449e479c90e81101d8717285667390f4
gdbtek <gdbtek@yahoo.com>
2017-03-03 22:26:00 -0800

    update version

--- cookbooks/selenium-server/attributes/hub.bash
+++ cookbooks/selenium-server/attributes/hub.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar'
+export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/3.2/selenium-server-standalone-3.2.0.jar'
 
 export SELENIUM_SERVER_INSTALL_FOLDER_PATH='/opt/selenium-server/hub'
 export SELENIUM_SERVER_JDK_INSTALL_FOLDER_PATH="${JDK_INSTALL_FOLDER_PATH}"
--- cookbooks/terraform/attributes/default.bash
+++ cookbooks/terraform/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.8.7/terraform_0.8.7_linux_amd64.zip'
+export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.8.8/terraform_0.8.8_linux_amd64.zip'
 
 export TERRAFORM_INSTALL_FOLDER_PATH='/opt/terraform'
