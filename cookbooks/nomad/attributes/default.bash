359db036a49d4f2ed2f0b3534b64d11d570e6967
gdbtek <gdbtek@yahoo.com>
2017-04-01 00:38:31 -0700

    update version

--- cookbooks/nomad/attributes/default.bash
+++ cookbooks/nomad/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.5.5/nomad_0.5.5_linux_amd64.zip'
+export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.5.6/nomad_0.5.6_linux_amd64.zip'
 
 export NOMAD_INSTALL_FOLDER_PATH='/opt/nomad'
--- cookbooks/terraform/attributes/default.bash
+++ cookbooks/terraform/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.9.1/terraform_0.9.1_linux_amd64.zip'
+export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.9.2/terraform_0.9.2_linux_amd64.zip'
 
 export TERRAFORM_INSTALL_FOLDER_PATH='/opt/terraform'
