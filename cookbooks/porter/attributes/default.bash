1b72da02488162d59655f34e1ac2cf5111b0ab2a
gdbtek <gdbtek@yahoo.com>
2017-04-01 00:02:07 -0700

    add porter

---
+++ cookbooks/porter/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+export PORTER_DOWNLOAD_URL='https://github.com/adobe-platform/porter/releases/download/v4.5.0/porter_linux386'
+
+export PORTER_INSTALL_FOLDER_PATH='/opt/porter'
---
+++ cookbooks/porter/templates/porter.sh.profile
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+export PATH="__INSTALL_FOLDER_PATH__/bin:${PATH}"
