7da772590ac777213ddf81372667945e0b3c24a9
gdbtek <gdbtek@yahoo.com>
2017-04-19 00:15:38 -0700

    update version

--- cookbooks/vbox-guest-additions/attributes/default.bash
+++ cookbooks/vbox-guest-additions/attributes/default.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export VBOX_GUEST_ADDITIONS_DOWNLOAD_URL='http://download.virtualbox.org/virtualbox/5.1.18/VBoxGuestAdditions_5.1.18.iso'
+export VBOX_GUEST_ADDITIONS_DOWNLOAD_URL='http://download.virtualbox.org/virtualbox/5.1.20/VBoxGuestAdditions_5.1.20.iso'
