2fcaad50afee8a081198e0466ac9aedb9fde0284
gdbtek <gdbtek@yahoo.com>
2017-04-27 02:04:21 -0700

    update version

--- cookbooks/terraform/attributes/default.bash
+++ cookbooks/terraform/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.9.3/terraform_0.9.3_linux_amd64.zip'
+export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.9.4/terraform_0.9.4_linux_amd64.zip'
 
 export TERRAFORM_INSTALL_FOLDER_PATH='/opt/terraform'
