47cc9d8e2a72965bb71ddd1d400f60504210116f
gdbtek <gdbtek@yahoo.com>
2017-05-09 09:45:56 -0700

    update version

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/0.8.1/consul_0.8.1_linux_amd64.zip'
+export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/0.8.2/consul_0.8.2_linux_amd64.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/opt/consul'
--- cookbooks/vault/attributes/default.bash
+++ cookbooks/vault/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.7.0/vault_0.7.0_linux_amd64.zip'
+export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.7.2/vault_0.7.2_linux_amd64.zip'
 
 export VAULT_INSTALL_FOLDER_PATH='/opt/vault'
