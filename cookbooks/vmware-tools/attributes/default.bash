1cdbc12a0966ef06b9b26e49db663536b27a97c9
Nam Nguyen <gdbtek@yahoo.com>
2017-10-10 23:36:59 -0700

    udpate path

--- cookbooks/vmware-tools/attributes/default.bash
+++ cookbooks/vmware-tools/attributes/default.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export VMWARE_TOOLS_DOWNLOAD_URL='https://dl.dropboxusercontent.com/u/15529567/downloads/linux/vmware-tools.tar.gz'
+export VMWARE_TOOLS_DOWNLOAD_URL='https://www.dropbox.com/s/1cwc4v3w7pv906h/vmware-tools.tar.gz'
