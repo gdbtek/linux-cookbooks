3fe7db62212dd389f0cd3fa4db004f1806dee10e
Nam Nguyen <gdbtek@yahoo.com>
2017-10-18 11:09:05 -0700

    udpate JDK version

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/9+181/jdk-9_linux-x64_bin.tar.gz'
+export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.tar.gz'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
