2c4c82ac3978504bc7fafff1c68aac7ecb125cb5
Nam Nguyen <gdbtek@yahoo.com>
2017-10-26 10:12:51 -0700

    add stackstorm

---
+++ cookbooks/stackstorm/attributes/default.bash
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+export STACKSTORM_ADMIN_LOGIN='st2admin'
+export STACKSTORM_ADMIN_PASSWORD='st2admin'
---
+++ cookbooks/stackstorm/recipes/install.bash
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    installPackage 'curl' 'curl'
+    installPackage 'nss' 'nss'
+}
+
+function install()
+{
+    umask '0022'
+
+    # Install
+
+    curl -sSL https://stackstorm.com/packages/install.sh |
+    bash -s -- --user="${STACKSTORM_ADMIN_LOGIN}" --password="${STACKSTORM_ADMIN_PASSWORD}"
+
+    umask '0077'
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+    source "${APP_FOLDER_PATH}/../attributes/default.bash"
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    header 'INSTALLING STACKSTORM'
+
+    installDependencies
+    install
+    installCleanUp
+}
+
+main "${@}"
