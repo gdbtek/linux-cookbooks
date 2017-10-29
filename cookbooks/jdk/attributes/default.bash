972238dc76dc338ab7d9e3628a44a0720a975715
Nam Nguyen <gdbtek@yahoo.com>
2017-10-28 23:51:25 -0700

    added jdk 1.8 link

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.tar.gz'
+# export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
