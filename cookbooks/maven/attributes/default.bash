7c1f609d4223701444f263aa793340897b3547b0
Nam Nguyen <gdbtek@yahoo.com>
2017-11-03 18:11:22 -0700

    update versions

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz'
+export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz'
 
 export MAVEN_INSTALL_FOLDER_PATH='/opt/maven'
 export MAVEN_JDK_INSTALL_FOLDER_PATH="${JDK_INSTALL_FOLDER_PATH}"
--- cookbooks/vbox-guest-additions/attributes/default.bash
+++ cookbooks/vbox-guest-additions/attributes/default.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export VBOX_GUEST_ADDITIONS_DOWNLOAD_URL='http://download.virtualbox.org/virtualbox/5.1.28/VBoxGuestAdditions_5.1.28.iso'
+export VBOX_GUEST_ADDITIONS_DOWNLOAD_URL='http://download.virtualbox.org/virtualbox/5.2.0/VBoxGuestAdditions_5.2.0.iso'
