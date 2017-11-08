e0c0df8171f0e475e45fcc44f61f20def035a85c
Nam Nguyen <gdbtek@yahoo.com>
2017-11-07 20:49:45 -0800

    add test-ssl cookbook

---
+++ cookbooks/test-ssl/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+export TEST_SSL_DOWNLOAD_URL='https://github.com/drwetter/testssl.sh/archive/v2.9.5-1.tar.gz'
+
+export TEST_SSL_INSTALL_FOLDER_PATH='/opt/test-ssl'
---
+++ cookbooks/test-ssl/recipes/install.bash
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+function install()
+{
+    umask '0022'
+
+    # Clean Up
+
+    initializeFolder "${TEST_SSL_INSTALL_FOLDER_PATH}"
+
+    # Install
+
+    unzipRemoteFile "${TEST_SSL_DOWNLOAD_URL}" "${TEST_SSL_INSTALL_FOLDER_PATH}"
+    chown -R "$(whoami):$(whoami)" "${TEST_SSL_INSTALL_FOLDER_PATH}"
+    ln -f -s "${TEST_SSL_INSTALL_FOLDER_PATH}/testssl.sh" '/usr/local/bin/testssl'
+
+    # Config Profile
+
+    local -r profileConfigData=('__INSTALL_FOLDER_PATH__' "${TEST_SSL_INSTALL_FOLDER_PATH}")
+
+    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/test-ssl.sh.profile" '/etc/profile.d/test-ssl.sh' "${profileConfigData[@]}"
+
+    # Display Version
+
+    displayVersion "$("${TEST_SSL_INSTALL_FOLDER_PATH}/testssl.sh" version)"
+
+    umask '0077'
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+    source "${APP_FOLDER_PATH}/../attributes/default.bash"
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    header 'INSTALLING TEST-SSL'
+
+    install
+    installCleanUp
+}
+
+main "${@}"
