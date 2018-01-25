3067197df1868b43ec84d1241f550058ea096ad7
Nam Nguyen <gdbtek@yahoo.com>
2018-01-25 10:26:14 -0800

    add clean-up tool

--- tools/clean-up.bash
+++ tools/clean-up.bash
@@ -4,10 +4,7 @@ function main()
 {
     source "$(dirname "${BASH_SOURCE[0]}")/../libraries/util.bash"
 
-    header 'UPGRADING INSTALLATION'
-    runUpgrade
-
-    "$(dirname "${BASH_SOURCE[0]}")/clean-up.bash"
+    installCleanUp
 }
 
 main "${@}"
