628e7d7cf4f684108229c405931721600dc7bceb
Nam Nguyen <gdbtek@yahoo.com>
2018-02-05 21:45:12 -0800

    add start http simple tool

---
+++ tools/start-http-server.bash
@@ -0,0 +1,89 @@
+#!/bin/bash -e
+
+#######################
+# MAIN IMPLEMENTATION #
+#######################
+
+function displayUsage()
+{
+    local -r scriptName="$(basename "${BASH_SOURCE[0]}")"
+
+    echo -e '\033[1;33m'
+    echo    'SYNOPSIS :'
+    echo    "  ${scriptName}"
+    echo    '    --help'
+    echo    '    --port    <PORT>'
+    echo -e '\033[1;35m'
+    echo    'DESCRIPTION :'
+    echo    '  --help    Help page (optional)'
+    echo    '  --port    Port number of server (require)'
+    echo -e '\033[1;36m'
+    echo    'EXAMPLES :'
+    echo    "  ./${scriptName} --help"
+    echo    "  ./${scriptName} --port '8080'"
+    echo -e '\033[0m'
+
+    exit "${1}"
+}
+
+function startHTTPServer()
+{
+    local -r port="${1}"
+
+    python -m 'SimpleHTTPServer' "${port}"
+}
+
+########
+# MAIN #
+########
+
+function main()
+{
+    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appFolderPath}/../../libraries/util.bash"
+
+    # Parsing Command Arguments
+
+    local -r optCount="${#}"
+
+    while [[ "${#}" -gt '0' ]]
+    do
+        case "${1}" in
+            --help)
+                displayUsage 0
+                ;;
+
+            --port)
+                shift
+
+                if [[ "${#}" -gt '0' ]]
+                then
+                    local port="${1}"
+                fi
+
+                ;;
+
+            *)
+                shift
+                ;;
+        esac
+    done
+
+    # Validate Opt
+
+    if [[ "${optCount}" -lt '1' ]]
+    then
+        displayUsage 0
+    fi
+
+    # Validate Port
+
+    checkNaturalNumber "${port}"
+
+    # Start Cleaning
+
+    startHTTPServer "${port}"
+}
+
+main "${@}"
