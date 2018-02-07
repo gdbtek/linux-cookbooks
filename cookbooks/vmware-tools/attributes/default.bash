90d66138a21f8e4f850d51a42b0ca11bd47dfcca
Nam Nguyen <gdbtek@yahoo.com>
2018-02-07 13:39:21 -0800

    update vmware tool download link

--- cookbooks/vmware-tools/attributes/default.bash
+++ cookbooks/vmware-tools/attributes/default.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export VMWARE_TOOLS_DOWNLOAD_URL='https://www.dropbox.com/s/1cwc4v3w7pv906h/vmware-tools.tar.gz'
+export VMWARE_TOOLS_DOWNLOAD_URL='https://www.dropbox.com/s/ui36bk2sknfqyar/vmware-tools.tar.gz'
