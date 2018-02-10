2943d57000d91eae9b4c638d885f97d25cbc5e1c
Nam Nguyen <gdbtek@yahoo.com>
2018-02-09 23:59:02 -0800

    update version

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/1.0.3/consul_1.0.3_linux_amd64.zip'
+export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/opt/consul'
