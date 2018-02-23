b51e90e771928b7fb37a5aca83fdaaff1577fc2c
Nam Nguyen <gdbtek@yahoo.com>
2018-02-23 10:37:42 -0800

    update version

--- cookbooks/vault/attributes/default.bash
+++ cookbooks/vault/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.9.3/vault_0.9.3_linux_amd64.zip'
+export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.9.4/vault_0.9.4_linux_amd64.zip'
 
 export VAULT_INSTALL_FOLDER_PATH='/opt/vault'
