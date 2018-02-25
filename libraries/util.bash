a586491e17a1de95954a3ed48d83debe2a8ca968
Nam Nguyen <gdbtek@yahoo.com>
2018-02-25 02:31:47 -0800

    add printTable function

--- libraries/util.bash
+++ libraries/util.bash
@@ -977,11 +977,82 @@ function isEmptyString()
     echo 'false' && return 1
 }
 
+function printTable()
+{
+    local -r delimiter="${1}"
+    local -r data="$(removeEmptyLines "${2}")"
+
+    local -r numberOfLines="$(wc -l <<< "${data}")"
+
+    if [[ "$(isEmptyString "${data}")" = 'false' && "${numberOfLines}" -gt '0' ]]
+    then
+        local table=''
+        local i=1
+
+        for ((i = 1; i <= "${numberOfLines}"; i = i + 1))
+        do
+            local line="$(sed "${i}q;d" <<< "${data}")"
+            local numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
+
+            # Add Line Delimiter
+
+            if [[ "${i}" -eq '1' ]]
+            then
+                table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+            fi
+
+            # Add Header Or Body
+
+            table="${table}\n"
+
+            local j=1
+
+            for ((j = 1; j <= "${numberOfColumns}"; j = j + 1))
+            do
+                table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
+            done
+
+            table="${table}#|\n"
+
+            # Add Line Delimiter
+
+            if [[ "${i}" -eq '1' ]]
+            then
+                table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+            fi
+
+            # Add Line Delimiter
+
+            if [[ "${numberOfLines}" -gt '1' && "${i}" -eq "${numberOfLines}" ]]
+            then
+                table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+            fi
+        done
+
+        if [[ "$(isEmptyString "${table}")" = 'false' ]]
+        then
+            echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'
+        fi
+    fi
+}
+
 function removeEmptyLines()
 {
     local -r content="${1}"
 
     echo -e "${content}" | sed '/^\s*$/d'
+}
+
+function repeatString()
+{
+    local -r string="${1}"
+    local -r numberToRepeat="${2}"
+
+    if [[ "$(isEmptyString "${string}")" = 'false' && "$(isNaturalNumber "${numberToRepeat}")" = 'true' && "${numberToRepeat}" -gt '0' ]]
+    then
+        local -r result="$(printf "%${numberToRepeat}s")"
+        echo -e "${result// /${string}}"
+    fi
 }
 
 function replaceString()
