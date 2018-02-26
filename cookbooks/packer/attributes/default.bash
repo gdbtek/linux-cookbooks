62e975c307c3df95b71d48727a879367379d3d09
Nam Nguyen <gdbtek@yahoo.com>
2018-02-25 22:58:28 -0800

    update version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.2.0/packer_1.2.0_linux_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip'
 
 export PACKER_INSTALL_FOLDER_PATH='/opt/packer'
