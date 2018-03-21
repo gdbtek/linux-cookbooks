0422ace11bcb67390cf6917f3a5c09d49d93745f
Nam Nguyen <gdbtek@yahoo.com>
2018-03-21 11:58:23 -0700

    update JDK

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.tar.gz'
+export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.tar.gz'
+# export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.tar.gz'
 # export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
