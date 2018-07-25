9bd09767146ff61105d6c1ee7eca28128778def8
Nam Nguyen <gdbtek@yahoo.com>
2018-07-25 09:30:45 -0700

    update version

--- cookbooks/groovy/attributes/default.bash
+++ cookbooks/groovy/attributes/default.bash
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export GROOVY_DOWNLOAD_URL='https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.0.zip'
+export GROOVY_DOWNLOAD_URL='https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.1.zip'
 
 export GROOVY_INSTALL_FOLDER_PATH='/opt/groovy'
 export GROOVY_JDK_INSTALL_FOLDER_PATH="${JDK_INSTALL_FOLDER_PATH}"
--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_linux-x64_bin.tar.gz'
-# export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.tar.gz'
+export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jdk-10.0.2_linux-x64_bin.tar.gz'
+# export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
