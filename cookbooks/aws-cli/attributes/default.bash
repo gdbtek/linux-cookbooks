da8ff39c2508dba9ad6fd8558fa850eb5d997f20
Nam Nguyen <gdbtek@yahoo.com>
2018-09-06 11:00:13 -0700

    update aws path

--- cookbooks/aws-cli/attributes/default.bash
+++ cookbooks/aws-cli/attributes/default.bash
@@ -2,4 +2,4 @@
 
 export AWS_CLI_DOWNLOAD_URL='https://s3.amazonaws.com/aws-cli/awscli-bundle.zip'
 
-export AWS_CLI_INSTALL_FOLDER_PATH='/opt/aws/aws-cli'
+export AWS_CLI_INSTALL_FOLDER_PATH='/opt/aws-cli'
