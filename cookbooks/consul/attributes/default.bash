acdff30c7f3cc6eb9812893cef43c5126b2a55ae
Nam Nguyen <gdbtek@yahoo.com>
2018-12-04 02:50:01 -0800

    update version

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://github.com/hashicorp/consul/archive/v1.3.0.zip'
+export CONSUL_DOWNLOAD_URL='https://github.com/hashicorp/consul/archive/v1.4.0.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/opt/consul'
--- cookbooks/node-js/attributes/default.bash
+++ cookbooks/node-js/attributes/default.bash
@@ -2,7 +2,7 @@
 
 export NODE_JS_INSTALL_FOLDER_PATH='/opt/node-js'
 
-# export NODE_JS_VERSION='v10.13.0'
+# export NODE_JS_VERSION='v10.14.1'
 export NODE_JS_VERSION='latest'
 
 export NODE_JS_INSTALL_NPM_PACKAGES=(
