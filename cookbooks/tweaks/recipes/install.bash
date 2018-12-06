b0fff503bb53a186cabd77198e6b4fe2eafd47c6
Nam Nguyen <gdbtek@yahoo.com>
2018-12-06 11:20:35 -0800

    bug fixes

--- cookbooks/tweaks/recipes/install.bash
+++ cookbooks/tweaks/recipes/install.bash
@@ -4,8 +4,8 @@ function main()
 {
     local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-    "${appFolderPath}/disable-hugepage"
-    "${appFolderPath}/disable-tcp-timestamp"
+    "${appFolderPath}/disable-hugepage.bash"
+    "${appFolderPath}/disable-tcp-timestamp.bash"
 }
 
 main "${@}"
