5d6dce7fcd81382a2038ed7e972ede43453a5cf3
Nam Nguyen <gdbtek@yahoo.com>
2018-12-06 15:56:30 -0800

    add tools

---
+++ tools/reset-logs.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+function main()
+{
+    source "$(dirname "${BASH_SOURCE[0]}")/../libraries/util.bash"
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    resetLogs "${@}"
+}
+
+main "${@}"
