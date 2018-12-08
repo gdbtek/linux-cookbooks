56dcb83327a78c8a5bf6bf86ca9c14cd48765251
Nam Nguyen <gdbtek@yahoo.com>
2018-12-08 00:17:46 -0800

    update version

--- cookbooks/akamai-cli/attributes/default.bash
+++ cookbooks/akamai-cli/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export AKAMAI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.0.2/akamai-1.0.2-linuxamd64'
+export AKAMAI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.1.0/akamai-1.1.0-linuxamd64'
 
 export AKAMAI_INSTALL_FOLDER_PATH='/opt/akamai'
--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip'
 
 export PACKER_INSTALL_FOLDER_PATH='/opt/packer'
