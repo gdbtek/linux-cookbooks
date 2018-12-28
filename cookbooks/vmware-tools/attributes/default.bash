45d8c48cf5b6f380b87dfe3e6d0693a3261a1d8c
Nam Nguyen <gdbtek@yahoo.com>
2018-12-28 01:03:40 -0800

    bug fixes

--- cookbooks/vmware-tools/attributes/default.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-export VMWARE_TOOLS_DOWNLOAD_URL='https://www.dropbox.com/s/ui36bk2sknfqyar/vmware-tools.tar.gz'
--- cookbooks/vmware-tools/recipes/install.bash
+++ cookbooks/vmware-tools/recipes/install.bash
@@ -1,22 +1,10 @@
 #!/bin/bash -e
 
-function installDependencies()
-{
-    installBuildEssential
-}
-
 function install()
 {
     umask '0022'
 
-    # Install
-
-    local -r tempFolder="$(getTemporaryFolder)"
-
-    unzipRemoteFile "${VMWARE_TOOLS_DOWNLOAD_URL}" "${tempFolder}"
-    cd "${tempFolder}"
-    "${tempFolder}/vmware-install.pl"
-    rm -f -r "${tempFolder}"
+    installPackages 'open-vm-tools'
 
     umask '0077'
 }
@@ -26,14 +14,12 @@ function main()
     local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     source "${appFolderPath}/../../../libraries/util.bash"
-    source "${appFolderPath}/../attributes/default.bash"
 
     checkRequireLinuxSystem
     checkRequireRootUser
 
     header 'INSTALLING VMWARE-TOOLS'
 
-    installDependencies
     install
     installCleanUp
 }
