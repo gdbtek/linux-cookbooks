954f622771d7131f2b002490bd4b897ead926a84
Nam Nguyen <gdbtek@yahoo.com>
2019-02-08 13:59:53 -0800

    update

---
+++ tools/clone-all-user-repositories.bash
@@ -0,0 +1,112 @@
+#!/bin/bash -e
+
+##################
+# IMPLEMENTATION #
+##################
+
+function displayUsage()
+{
+    local -r scriptName="$(basename "${BASH_SOURCE[0]}")"
+
+    echo -e '\033[1;33m'
+    echo    'SYNOPSIS :'
+    echo    "  ${scriptName}"
+    echo    '    --help'
+    echo    '    --user            <USER>'
+    echo    '    --token           <TOKEN>'
+    echo    '    --clone-folder    <CLONE_FOLDER>'
+    echo -e '\033[1;35m'
+    echo    'DESCRIPTION :'
+    echo    '  --help            Help page (optional)'
+    echo    '  --user            User name (require)'
+    echo    '  --token           Personal access token (require)'
+    echo    '  --clone-folder    Folder path to clone all repositories to (require)'
+    echo -e '\033[1;36m'
+    echo    'EXAMPLES :'
+    echo    "  ./${scriptName} --help"
+    echo    "  ./${scriptName} --user 'gdbtek' --token 'a5hb5ds1cfq0d1p8brgmspnogdib9hfn7kcy2xaf' --clone-folder='/path/to/folder'"
+
+    echo -e '\033[0m'
+
+    exit "${1}"
+}
+
+function cloneAllUserRepositories()
+{
+    local -r user="${1}"
+    local -r token="${2}"
+    local -r cloneFolder="${3}"
+
+    checkNonEmptyString "${user}" 'undefined user'
+    checkNonEmptyString "${token}" 'undefined token'
+    checkExistFolder "${cloneFolder}"
+}
+
+########
+# MAIN #
+########
+
+function main()
+{
+    source "$(dirname "${BASH_SOURCE[0]}")/../libraries/util.bash"
+
+    # Parsing Command Arguments
+
+    local -r optCount="${#}"
+
+    while [[ "${#}" -gt '0' ]]
+    do
+        case "${1}" in
+            --help)
+                displayUsage 0
+                ;;
+
+            --user)
+                shift
+
+                if [[ "${#}" -gt '0' ]]
+                then
+                    local user="${1}"
+                fi
+
+                ;;
+
+            --token)
+                shift
+
+                if [[ "${#}" -gt '0' ]]
+                then
+                    local token="${1}"
+                fi
+
+                ;;
+
+            --clone-folder)
+                shift
+
+                if [[ "${#}" -gt '0' ]]
+                then
+                    local cloneFolder="${1}"
+                fi
+
+                ;;
+
+            *)
+                shift
+                ;;
+        esac
+    done
+
+    # Validate Opt
+
+    if [[ "${optCount}" -lt '1' ]]
+    then
+        displayUsage 0
+    fi
+
+    # Clone Repositories
+
+    cloneAllUserRepositories "${user}" "${token}" "${cloneFolder}"
+}
+
+main "${@}"
