919f87ddc896795f165b730d4aec2e2bd3ddf0d1
Nam Nguyen <gdbtek@yahoo.com>
2019-03-13 12:58:29 -0700

    update version

--- cookbooks/go-cd/attributes/default.bash
+++ cookbooks/go-cd/attributes/default.bash
@@ -2,8 +2,8 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
 
-export GO_CD_AGENT_DOWNLOAD_URL='https://download.gocd.org/binaries/19.1.0-8469/generic/go-agent-19.1.0-8469.zip'
-export GO_CD_SERVER_DOWNLOAD_URL='https://download.gocd.org/binaries/19.1.0-8469/generic/go-server-19.1.0-8469.zip'
+export GO_CD_AGENT_DOWNLOAD_URL='https://download.gocd.org/binaries/19.2.0-8641/generic/go-agent-19.2.0-8641.zip'
+export GO_CD_SERVER_DOWNLOAD_URL='https://download.gocd.org/binaries/19.2.0-8641/generic/go-server-19.2.0-8641.zip'
 
 export GO_CD_AGENT_INSTALL_FOLDER_PATH='/opt/go-cd/agents/agent'
 export GO_CD_JDK_INSTALL_FOLDER_PATH="${JDK_INSTALL_FOLDER_PATH}"
