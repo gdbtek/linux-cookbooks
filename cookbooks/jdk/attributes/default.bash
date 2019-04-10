e9a8e576aef725f289290a3d0783dec26c2ffee5
Nam Nguyen <gdbtek@yahoo.com>
2019-04-10 16:27:57 -0700

    update version

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz'
+export JDK_DOWNLOAD_URL='https://download.oracle.com/otn-pub/java/jdk/12+33/312335d836a34c7c8bba9d963e26dc23/jdk-12_linux-x64_bin.tar.gz'
+# export JDK_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz'
 # export JDK_DOWNLOAD_URL='https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.tar.gz'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
--- cookbooks/splunk-forwarder/attributes/default.bash
+++ cookbooks/splunk-forwarder/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export SPLUNK_FORWARDER_DOWNLOAD_URL='https://download.splunk.com/products/universalforwarder/releases/7.2.4/linux/splunkforwarder-7.2.4-8a94541dcfac-Linux-x86_64.tgz'
+export SPLUNK_FORWARDER_DOWNLOAD_URL='https://download.splunk.com/products/universalforwarder/releases/7.2.5.1/linux/splunkforwarder-7.2.5.1-962d9a8e1586-Linux-x86_64.tgz'
 export SPLUNK_FORWARDER_INSTALL_FOLDER_PATH='/opt/splunkforwarder'
 
 export SPLUNK_FORWARDER_USER_NAME='splunk'
