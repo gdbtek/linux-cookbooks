e0eed01fa1f91e1adfa5f9550fc160b9848054ec
Nam Nguyen <gdbtek@yahoo.com>
2019-06-06 09:15:21 -0700

    update jdk download path

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 export JDK_DOWNLOAD_URL='https://download.oracle.com/otn-pub/java/jdk/12.0.1+12/69cfe15208a647278a19ef0990eea691/jdk-12.0.1_linux-x64_bin.tar.gz'
-# export JDK_DOWNLOAD_URL='https://download.oracle.com/otn/java/jdk/11.0.3+12/37f5e150db5247ab9333b11c1dddcd30/jdk-11.0.3_linux-x64_bin.tar.gz'
-# export JDK_DOWNLOAD_URL='https://download.oracle.com/otn/java/jdk/8u211-b12/478a62b7d4e34b78b671c754eaaf38ab/jdk-8u211-linux-x64.tar.gz'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/lsuabe808jf8ppx/jdk-11.0.3_linux-x64_bin.tar.gz?dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/ln3hx0jrpx5qjgu/jdk-8u211-linux-x64.tar.gz?dl=1'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
