dc7c06ce74d248a827bc42ba25a997854aace845
Nam Nguyen <gdbtek@yahoo.com>
2019-06-09 22:19:50 -0700

    update

--- cookbooks/akamai-cli/attributes/default.bash
+++ cookbooks/akamai-cli/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export AKAMAI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.1.3/akamai-1.1.3-linuxamd64'
+export AKAMAI_CLI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.1.4/akamai-1.1.4-linuxamd64'
 
-export AKAMAI_INSTALL_FOLDER_PATH='/opt/akamai'
+export AKAMAI_CLI_INSTALL_FOLDER_PATH='/opt/akamai'
--- cookbooks/akamai-cli/recipes/install.bash
+++ cookbooks/akamai-cli/recipes/install.bash
@@ -1,48 +1,11 @@
 #!/bin/bash -e
 
-function install()
-{
-    umask '0022'
-
-    # Clean Up
-
-    initializeFolder "${AKAMAI_INSTALL_FOLDER_PATH}"
-    initializeFolder "${AKAMAI_INSTALL_FOLDER_PATH}/bin"
-
-    # Install
-
-    downloadFile "${AKAMAI_DOWNLOAD_URL}" "${AKAMAI_INSTALL_FOLDER_PATH}/bin/akamai" 'true'
-    chown -R "$(whoami):$(whoami)" "${AKAMAI_INSTALL_FOLDER_PATH}"
-    chmod 755 "${AKAMAI_INSTALL_FOLDER_PATH}/bin/akamai"
-    ln -s -f "${AKAMAI_INSTALL_FOLDER_PATH}/bin/akamai" '/usr/bin/akamai'
-
-    # Config Profile
-
-    local -r profileConfigData=('__INSTALL_FOLDER_PATH__' "${AKAMAI_INSTALL_FOLDER_PATH}")
-
-    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/akamai.sh.profile" '/etc/profile.d/akamai.sh' "${profileConfigData[@]}"
-
-    # Display Version
-
-    displayVersion "$(akamai --version)"
-
-    umask '0077'
-}
-
 function main()
 {
-    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
+    source "$(dirname "${BASH_SOURCE[0]}")/../attributes/default.bash"
 
-    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
-    source "${APP_FOLDER_PATH}/../attributes/default.bash"
-
-    checkRequireLinuxSystem
-    checkRequireRootUser
-
-    header 'INSTALLING AKAMAI-CLI'
-
-    install
-    installCleanUp
+    installPortableBinary 'AKAMAI-CLI' "${AKAMAI_CLI_DOWNLOAD_URL}" "${AKAMAI_CLI_INSTALL_FOLDER_PATH}" 'akamai' '--version'
 }
 
 main "${@}"
