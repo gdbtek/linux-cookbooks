117a99df3407a0bc42cf509dcbad4a43af3cef3f
Nam Nguyen <gdbtek@yahoo.com>
2019-06-09 23:10:49 -0700

    update

--- cookbooks/akamai-cli/attributes/default.bash
+++ cookbooks/akamai-cli/attributes/default.bash
@@ -2,4 +2,4 @@
 
 export AKAMAI_CLI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.1.4/akamai-1.1.4-linuxamd64'
 
-export AKAMAI_CLI_INSTALL_FOLDER_PATH='/opt/akamai'
+export AKAMAI_CLI_INSTALL_FOLDER_PATH='/opt/akamai-cli'
