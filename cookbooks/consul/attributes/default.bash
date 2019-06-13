6e7c23d079e9fbd4afaa763ee43a049a4dc77d7e
Nam Nguyen <gdbtek@yahoo.com>
2019-06-13 15:00:46 -0700

    bug fixes

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://github.com/hashicorp/consul/archive/v1.5.1.zip'
+export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/1.5.1/consul_1.5.1_linux_amd64.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/opt/consul'
