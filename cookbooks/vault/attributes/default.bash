54e5fe67e37c2b33e63f9f7b925679bcd9d6c60a
Nam Nguyen <gdbtek@yahoo.com>
2019-06-13 13:15:55 -0700

    bug fixes

--- cookbooks/vault/attributes/default.bash
+++ cookbooks/vault/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export VAULT_DOWNLOAD_URL='https://github.com/hashicorp/vault/archive/v1.1.3.tar.gz'
+export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/1.1.3/vault_1.1.3_linux_amd64.zip'
 
 export VAULT_INSTALL_FOLDER_PATH='/opt/vault'
