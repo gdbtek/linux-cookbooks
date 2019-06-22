8ce0d53aa30599690adaf669e85ada36c56bd54b
Nam Nguyen <gdbtek@yahoo.com>
2019-06-22 01:28:26 -0700

    bug fixes

--- cookbooks/data-dog/recipes/install.bash
+++ cookbooks/data-dog/recipes/install.bash
@@ -5,8 +5,8 @@ function install()
     umask '0022'
 
     export DD_API_KEY="${DATA_DOG_API_KEY}"
-    bash -c -e "$(curl -s -L "${DATA_DOG_DOWNLOAD_URL}" --retry 12 --retry-delay 5)"
-    statusService 'datadog-agent'
+    curl -s -L "${DATA_DOG_DOWNLOAD_URL}" --retry 12 --retry-delay 5 | bash -e
+    restartService 'datadog-agent'
 
     umask '0077'
 }
