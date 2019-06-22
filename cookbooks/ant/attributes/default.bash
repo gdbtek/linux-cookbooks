2af286cedc063673bd5b5513af75d60090d5034a
Nam Nguyen <gdbtek@yahoo.com>
2019-06-22 00:26:03 -0700

    bug fixes

--- cookbooks/ant/attributes/default.bash
+++ cookbooks/ant/attributes/default.bash
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-source "$(dirname "${BASH_SOURCE[0]}")/../../jdk/attributes/default.bash"
-
 export ANT_DOWNLOAD_URL='http://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.6-bin.tar.gz'
 
 export ANT_INSTALL_FOLDER_PATH='/opt/ant'
-export ANT_JDK_INSTALL_FOLDER_PATH="${JDK_INSTALL_FOLDER_PATH}"
