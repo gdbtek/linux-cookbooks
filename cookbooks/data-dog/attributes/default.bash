ba0c1bf845aa01a124788ef07564a53bc6897c3d
Nam Nguyen <gdbtek@yahoo.com>
2019-06-23 22:31:49 -0700

    bug fixes

--- cookbooks/data-dog/attributes/default.bash
+++ cookbooks/data-dog/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export DATA_DOG_DOWNLOAD_URL='https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh'
+export DATA_DOG_DOWNLOAD_URL='https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh'
 
 export DATA_DOG_API_KEY='00000000000000000000000000000000'
