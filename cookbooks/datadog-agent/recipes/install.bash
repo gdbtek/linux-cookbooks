c80dd818e50ec9580d4fe80de58776ffcf61c8a7
Nam Nguyen <gdbtek@yahoo.com>
2019-06-23 22:47:36 -0700

    bug fixes

--- cookbooks/datadog-agent/recipes/install.bash
+++ cookbooks/datadog-agent/recipes/install.bash
@@ -7,6 +7,7 @@ function install()
     export DD_API_KEY="${DATADOG_AGENT_API_KEY}"
     curl -s -L "${DATADOG_AGENT_DOWNLOAD_URL}" --retry 12 --retry-delay 5 | bash -e
     restartService 'datadog-agent'
+    rm -f "$(pwd)/ddagent-install.log"
 
     umask '0077'
 }
