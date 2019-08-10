a36f89ca4d72ed0719436766e210125801526841
Nam Nguyen <gdbtek@yahoo.com>
2019-08-10 13:26:27 -0700

    updates

--- tools/clean-up-jenkins-jobs.bash
+++ tools/clean-up-jenkins-jobs.bash
@@ -42,6 +42,103 @@ function displayUsage()
     exit "${1}"
 }
 
+function cleanUpDotBuildFolder()
+{
+    local -r buildsFolderPath="${1}"
+    local -r commandMode="${2}"
+
+    # Dot Builds (builds/.12345)
+
+    local dotBuilds="$(find "${buildsFolderPath}" -mindepth 1 -maxdepth 1 -type d -regex "^${buildsFolderPath}/\.[1-9][0-9]*$" -exec basename '{}' \;)"
+
+    if [[ "$(isEmptyString "${dotBuilds}")" = 'false' ]]
+    then
+        NEED_TO_CLEAN_UP_DOT_BUILDS='true'
+
+        info "\n${buildsFolderPath}"
+
+        if [[ "${commandMode}" = 'clean-up' ]]
+        then
+            echo -e "  \033[1;35mdeleting dot-builds :\033[0m"
+        else
+            echo -e "  \033[1;35mto delete dot-builds :\033[0m"
+        fi
+
+        local dotBuild=''
+
+        for dotBuild in ${dotBuilds}
+        do
+            echo "    '${buildsFolderPath}/${dotBuild}'"
+
+            if [[ "${commandMode}" = 'clean-up' ]]
+            then
+                rm -f -r "${buildsFolderPath}/${dotBuild}"
+            fi
+        done
+    fi
+}
+
+function cleanUpBuildFolder()
+{
+    local -r buildsFolderPath="${1}"
+    local -r commandMode="${2}"
+    local -r numberBuildsToKeep="${3}"
+
+    # Normal Builds (builds/12345)
+
+    local builds="$(find "${buildsFolderPath}" -mindepth 1 -maxdepth 1 -type d -regex "^${buildsFolderPath}/[1-9][0-9]*$" -exec basename '{}' \; | sort -n -r)"
+    local toDeleteBuilds="$(tail -n "+$((numberBuildsToKeep + 1))" <<< "${builds}")"
+    local toKeepBuilds="$(head "-${numberBuildsToKeep}" <<< "${builds}")"
+
+    if [[ "$(isEmptyString "${toDeleteBuilds}")" = 'false' ]]
+    then
+        NEED_TO_CLEAN_UP_BUILDS='true'
+
+        info "\n${buildsFolderPath}"
+
+        # Print To Keep If Available
+
+        if [[ "$(isEmptyString "${toKeepBuilds}")" = 'false' ]]
+        then
+            echo -e "  \033[1;32mto keep builds :\033[0m"
+
+            local toKeepBuild=''
+
+            for toKeepBuild in ${toKeepBuilds}
+            do
+                checkPositiveInteger "${toKeepBuild}"
+
+                echo "    '${buildsFolderPath}/${toKeepBuild}'"
+            done
+        fi
+
+        # Print To Delete As Always
+
+        if [[ "${commandMode}" = 'clean-up' ]]
+        then
+            echo -e "  \033[1;35mdeleting builds :\033[0m"
+        else
+            echo -e "  \033[1;35mto delete builds :\033[0m"
+        fi
+
+        # Delete
+
+        local toDeleteBuild=''
+
+        for toDeleteBuild in ${toDeleteBuilds}
+        do
+            checkPositiveInteger "${toDeleteBuild}"
+
+            echo "    '${buildsFolderPath}/${toDeleteBuild}'"
+
+            if [[ "${commandMode}" = 'clean-up' ]]
+            then
+                rm -f -r "${buildsFolderPath}/${toDeleteBuild}"
+            fi
+        done
+    fi
+}
+
 function cleanJenkinsJobs()
 {
     local -r jobsFolderPath="${1}"
@@ -51,105 +148,25 @@ function cleanJenkinsJobs()
     local -r oldIFS="${IFS}"
     IFS=$'\n'
 
-    local needToCleanUpDotBuilds='false'
-    local needToCleanUpBuilds='false'
+    NEED_TO_CLEAN_UP_DOT_BUILDS='false'
+    NEED_TO_CLEAN_UP_BUILDS='false'
+
     local buildsFolderPath=''
 
     for buildsFolderPath in $(find "${jobsFolderPath}" -mindepth 1 -maxdepth 4 -type d -name 'builds')
     do
-        # Dot Builds (builds/.12345)
-
-        local dotBuilds="$(find "${buildsFolderPath}" -mindepth 1 -maxdepth 1 -type d -regex "^${buildsFolderPath}/\.[1-9][0-9]*$" -exec basename '{}' \;)"
-
-        if [[ "$(isEmptyString "${dotBuilds}")" = 'false' ]]
-        then
-            needToCleanUpDotBuilds='true'
-
-            info "\n${buildsFolderPath}"
-
-            if [[ "${commandMode}" = 'clean-up' ]]
-            then
-                echo -e "  \033[1;35mdeleting dot-builds :\033[0m"
-            else
-                echo -e "  \033[1;35mto delete dot-builds :\033[0m"
-            fi
-
-            local dotBuild=''
-
-            for dotBuild in ${dotBuilds}
-            do
-                echo "    '${buildsFolderPath}/${dotBuild}'"
-
-                if [[ "${commandMode}" = 'clean-up' ]]
-                then
-                    rm -f -r "${buildsFolderPath}/${dotBuild}"
-                fi
-            done
-        fi
-
-        # Normal Builds (builds/12345)
-
-        local builds="$(find "${buildsFolderPath}" -mindepth 1 -maxdepth 1 -type d -regex "^${buildsFolderPath}/[1-9][0-9]*$" -exec basename '{}' \; | sort -n -r)"
-        local toDeleteBuilds="$(tail -n "+$((numberBuildsToKeep + 1))" <<< "${builds}")"
-        local toKeepBuilds="$(head "-${numberBuildsToKeep}" <<< "${builds}")"
-
-        if [[ "$(isEmptyString "${toDeleteBuilds}")" = 'false' ]]
-        then
-            needToCleanUpBuilds='true'
-
-            info "\n${buildsFolderPath}"
-
-            # Print To Keep If Available
-
-            if [[ "$(isEmptyString "${toKeepBuilds}")" = 'false' ]]
-            then
-                echo -e "  \033[1;32mto keep builds :\033[0m"
-
-                local toKeepBuild=''
-
-                for toKeepBuild in ${toKeepBuilds}
-                do
-                    checkPositiveInteger "${toKeepBuild}"
-
-                    echo "    '${buildsFolderPath}/${toKeepBuild}'"
-                done
-            fi
-
-            # Print To Delete As Always
-
-            if [[ "${commandMode}" = 'clean-up' ]]
-            then
-                echo -e "  \033[1;35mdeleting builds :\033[0m"
-            else
-                echo -e "  \033[1;35mto delete builds :\033[0m"
-            fi
-
-            # Delete
-
-            local toDeleteBuild=''
-
-            for toDeleteBuild in ${toDeleteBuilds}
-            do
-                checkPositiveInteger "${toDeleteBuild}"
-
-                echo "    '${buildsFolderPath}/${toDeleteBuild}'"
-
-                if [[ "${commandMode}" = 'clean-up' ]]
-                then
-                    rm -f -r "${buildsFolderPath}/${toDeleteBuild}"
-                fi
-            done
-        fi
+        cleanUpDotBuildFolder "${buildsFolderPath}" "${commandMode}"
+        cleanUpBuildFolder "${buildsFolderPath}" "${commandMode}" "${numberBuildsToKeep}"
     done
 
     IFS="${oldIFS}"
 
-    if [[ "${needToCleanUpDotBuilds}" = 'false' ]]
+    if [[ "${NEED_TO_CLEAN_UP_DOT_BUILDS}" = 'false' ]]
     then
         echo -e "\n\033[1;32mno dot-build to clean up!\033[0m"
     fi
 
-    if [[ "${needToCleanUpBuilds}" = 'false' ]]
+    if [[ "${NEED_TO_CLEAN_UP_BUILDS}" = 'false' ]]
     then
         echo -e "\n\033[1;32mno build to clean up!\033[0m"
     fi
