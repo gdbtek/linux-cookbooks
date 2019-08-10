7dca16a2621aab597fa4126807fe6d028f25589e
Nam Nguyen <gdbtek@yahoo.com>
2019-08-10 14:24:33 -0700

    bug fixes

--- tools/clean-up-jenkins-jobs.bash
+++ tools/clean-up-jenkins-jobs.bash
@@ -49,7 +49,7 @@ function cleanUpDotBuildFolder()
 
     # Dot Builds (builds/.12345)
 
-    local dotBuilds="$(find "${buildsFolderPath}" -mindepth 1 -maxdepth 1 -type d -regex "^${buildsFolderPath}/\.[1-9][0-9]*$" -exec basename '{}' \;)"
+    local dotBuilds="$(find "${buildsFolderPath}" -mindepth 1 -maxdepth 1 \( -type d -o -type l \) -regex "^${buildsFolderPath}/\.[1-9][0-9]*$" -exec basename '{}' \;)"
 
     if [[ "$(isEmptyString "${dotBuilds}")" = 'false' ]]
     then
