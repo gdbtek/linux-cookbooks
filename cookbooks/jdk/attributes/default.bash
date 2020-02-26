0517407b866b98aa69ea355f308b1d9e94d7c408
Nam Nguyen <gdbtek@yahoo.com>
2020-02-26 10:26:49 -0800

    update version

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='https://download.oracle.com/otn-pub/java/jdk/13.0.1+9/cec27d702aa74d5a8630c65ae61e4305/jdk-13.0.1_linux-x64_bin.tar.gz'
+export JDK_DOWNLOAD_URL='https://download.oracle.com/otn-pub/java/jdk/13.0.2+8/d4173c853231432d94f001e99d882ca7/jdk-13.0.2_linux-x64_bin.tar.gz'
 # export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/k3eztzug8e8htpg/jdk-11.0.5_linux-x64_bin.tar.gz?dl=1'
 # export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/f9wymhw1w878mho/jdk-8u231-linux-x64.tar.gz?dl=1'
 
