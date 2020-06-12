06a506ebf6ed1efb9c7f93293885ea899b87dfcf
Nam Nguyen <gdbtek@yahoo.com>
2020-06-12 14:14:43 -0700

    update jdk

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='https://download.oracle.com/otn-pub/java/jdk/13.0.2+8/d4173c853231432d94f001e99d882ca7/jdk-13.0.2_linux-x64_bin.tar.gz'
-# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/k3eztzug8e8htpg/jdk-11.0.5_linux-x64_bin.tar.gz?dl=1'
-# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/f9wymhw1w878mho/jdk-8u231-linux-x64.tar.gz?dl=1'
+export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/ujev7ah3818m1qv/jdk-14.0.1_linux-x64_bin.tar.gz?dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/ypufbecfbz7jqp8/jdk-11.0.7_linux-x64_bin.tar.gz?dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/7565esq7oge9zdi/jdk-8u251-linux-i586.tar.gz?dl=1'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
