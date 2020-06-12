ec0d2cf1d2814e59c274b80428a8dbb40f8a50d9
Nam Nguyen <gdbtek@yahoo.com>
2020-06-12 12:19:07 -0700

    update

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz'
+export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz'
 
 export MAVEN_INSTALL_FOLDER_PATH='/opt/maven'
