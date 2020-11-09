6da67738ca88c4e814d709fa63d0fd52857d7cff
Nam Nguyen <gdbtek@yahoo.com>
2020-11-09 10:08:47 -0800

    update akamai version

--- cookbooks/akamai-cli/attributes/default.bash
+++ cookbooks/akamai-cli/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export AKAMAI_CLI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.1.4/akamai-1.1.4-linuxamd64'
+export AKAMAI_CLI_DOWNLOAD_URL='https://github.com/akamai/cli/releases/download/1.1.5/akamai-1.1.5-linuxamd64'
 
 export AKAMAI_CLI_INSTALL_FOLDER_PATH='/opt/akamai-cli'
