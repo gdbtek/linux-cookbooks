5b05f2f694727d428cd1e1c59fe68abe597aebbd
Nam Nguyen <gdbtek@yahoo.com>
2021-05-19 13:11:01 -0700

    update lib

--- libraries/aws.bash
+++ libraries/aws.bash
@@ -358,7 +358,7 @@ function checkValidRegion()
 
 function getAllowedRegions()
 {
-    echo 'af-south-1 ap-east-1 ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-south-1 eu-west-1 eu-west-2 eu-west-3 me-south-1 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2'
+    echo 'af-south-1 ap-east-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-south-1 eu-west-1 eu-west-2 eu-west-3 me-south-1 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2'
 }
 
 function getRegionFromRecordSetAliasTargetDNSName()
@@ -397,6 +397,9 @@ function getShortRegionName()
     elif [[ "${region}" = 'ap-northeast-2' ]]
     then
         echo 'apne2'
+    elif [[ "${region}" = 'ap-northeast-3' ]]
+    then
+        echo 'apne3'
     elif [[ "${region}" = 'ap-south-1' ]]
     then
         echo 'aps1'
