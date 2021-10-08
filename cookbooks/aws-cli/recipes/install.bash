6d226c3b31373fac469c8432162bebd63e158810
Nam Nguyen <gdbtek@yahoo.com>
2021-10-07 22:30:38 -0700

    bug fixes

--- cookbooks/aws-cli/recipes/install.bash
+++ cookbooks/aws-cli/recipes/install.bash
@@ -13,9 +13,9 @@ function install()
         -b '/usr/bin/aws' \
         -i "${AWS_CLI_INSTALL_FOLDER_PATH}"
     rm -f -r "${tempFolder}"
-    chown -R "$(whoami):$(whoami)" "${AWS_CLI_INSTALL_FOLDER_PATH}"
-    symlinkListUsrBin "${AWS_CLI_INSTALL_FOLDER_PATH}/bin/aws"
-    displayVersion "$('/usr/bin/aws' --version 2>&1)"
+    #chown -R "$(whoami):$(whoami)" "${AWS_CLI_INSTALL_FOLDER_PATH}"
+    #symlinkListUsrBin "${AWS_CLI_INSTALL_FOLDER_PATH}/bin/aws"
+    displayVersion "$('/usr/bin/aws/aws' --version 2>&1)"
 
     umask '0077'
 }
