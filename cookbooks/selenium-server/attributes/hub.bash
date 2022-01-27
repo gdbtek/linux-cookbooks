7856bdb80e875854fa14c7e7c306f9d6f867d7ee
Nam Nguyen <gdbtek@yahoo.com>
2022-01-27 10:41:27 -0800

    update

--- cookbooks/selenium-server/attributes/hub.bash
+++ cookbooks/selenium-server/attributes/hub.bash
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-export SELENIUM_SERVER_DOWNLOAD_URL='http://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar'
+export SELENIUM_SERVER_DOWNLOAD_URL='https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-4.1.1.jar'
 
-export SELENIUM_SERVER_INSTALL_FOLDER_PATH='/opt/selenium-server/hub'
+export SELENIUM_SERVER_INSTALL_FOLDER_PATH='/opt/selenium-server'
 
-export SELENIUM_SERVER_SERVICE_NAME='selenium-server-hub'
+export SELENIUM_SERVER_SERVICE_NAME='selenium-server'
 
 export SELENIUM_SERVER_USER_NAME='selenium'
 export SELENIUM_SERVER_GROUP_NAME='selenium'
