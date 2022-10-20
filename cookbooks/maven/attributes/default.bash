b8a677793615e23f57ad1af08ebe9fc537f52464
Nam Nguyen <gdbtek@yahoo.com>
2022-10-20 14:48:08 -0700

    update maven

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export MAVEN_DOWNLOAD_URL='https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz'
+export MAVEN_DOWNLOAD_URL='https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz'
 
 export MAVEN_INSTALL_FOLDER_PATH='/opt/maven'
