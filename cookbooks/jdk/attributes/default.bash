81fe173f919feaee467e0971d9a8f2b215b4b35c
Nam Nguyen <gdbtek@yahoo.com>
2023-02-02 15:17:47 -0800

    update jdk

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/4o3ntatmvplohnk/jdk-17_linux-x64_bin.tar.gz?dl=1'
+export JDK_DOWNLOAD_URL='https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz'
 # export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/liw5aqkl7jb2oc2/jdk-11.0.14_linux-x64_bin.tar.gz?dl=1'
 # export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/ku8h5u991rrnhsd/jdk-8u321-linux-x64.tar.gz?dl=1'
 
