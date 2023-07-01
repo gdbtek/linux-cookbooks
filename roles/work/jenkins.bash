89262eb87c1e52798da89288051d3c4dbc4a5870
Nam Nguyen <gdbtek@yahoo.com>
2023-07-01 00:18:53 -0700

    update

--- roles/work/jenkins.bash
+++ roles/work/jenkins.bash
@@ -17,12 +17,12 @@ function main()
 
     source "$(dirname "${BASH_SOURCE[0]}")/../../libraries/util.bash"
 
-    "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/clean-up/recipes/install.bash"
-    "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/jq/recipes/install.bash"
-    "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/logrotate/recipes/install.bash"
-    "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/ps1/recipes/install.bash" --host-name "${ps1HostName}" --users "${ps1Users}"
-    "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/vim/recipes/install.bash"
-    "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/chrony/recipes/install.bash"
+    # "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/clean-up/recipes/install.bash"
+    # "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/jq/recipes/install.bash"
+    # "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/logrotate/recipes/install.bash"
+    # "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/ps1/recipes/install.bash" --host-name "${ps1HostName}" --users "${ps1Users}"
+    # "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/vim/recipes/install.bash"
+    # "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/chrony/recipes/install.bash"
 
     configureJenkinsCI
 
