65b109bee61f29a03a522c8dddb11999b449d64f
Nam Nguyen <gdbtek@yahoo.com>
2024-04-30 21:59:08 -0700

    update jq version

--- cookbooks/jq/attributes/default.bash
+++ cookbooks/jq/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export JQ_DOWNLOAD_URL='https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64'
+export JQ_DOWNLOAD_URL='https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-i386'
 
 export JQ_INSTALL_FOLDER_PATH='/opt/jq'
