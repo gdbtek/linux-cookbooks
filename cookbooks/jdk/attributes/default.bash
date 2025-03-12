53861eaa1a940d0a257122f783def576de1048e1
Nam Nguyen <gdbtek@yahoo.com>
2025-03-11 22:25:20 -0700

    update jdk version

--- cookbooks/jdk/attributes/default.bash
+++ cookbooks/jdk/attributes/default.bash
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-export JDK_DOWNLOAD_URL='https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz'
-# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/liw5aqkl7jb2oc2/jdk-11.0.14_linux-x64_bin.tar.gz?dl=1'
-# export JDK_DOWNLOAD_URL='https://www.dropbox.com/s/ku8h5u991rrnhsd/jdk-8u321-linux-x64.tar.gz?dl=1'
+export JDK_DOWNLOAD_URL='https://www.dropbox.com/scl/fi/kx80nxd0uno0jwai4v7xw/jdk-23_linux-x64_bin.tar.gz?rlkey=il208c7isivlkkqo2uj02d1y5&dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/scl/fi/4fpj1h764m7nlsgs02n0t/jdk-21_linux-x64_bin.tar.gz?rlkey=8h43zzby8s269ixkfw207x3lz&dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/scl/fi/vvju215ky27bci8itpm5z/jdk-17.0.14_linux-x64_bin.tar.gz?rlkey=4zfnzm66kxaad4zw3r6c6qq6j&dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/scl/fi/u9suklxb7xj741zirvcm7/jdk-11.0.26_linux-x64_bin.tar.gz?rlkey=rrupf7tuyomzq9gxee75jk17h&dl=1'
+# export JDK_DOWNLOAD_URL='https://www.dropbox.com/scl/fi/ntuetb2ckwda2g3y2hgok/jdk-8u441-linux-x64.tar.gz?rlkey=488i4u37fzb053fpq8y2kzand&dl=1'
 
 export JDK_INSTALL_FOLDER_PATH='/opt/jdk'
