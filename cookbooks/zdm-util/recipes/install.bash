4bc608f0c6cfad963945dc01cc66835092583acc
Nam Nguyen <gdbtek@yahoo.com>
2025-05-05 15:58:57 -0700

    bug fixes

--- cookbooks/zdm-util/recipes/install.bash
+++ cookbooks/zdm-util/recipes/install.bash
@@ -5,13 +5,14 @@ function main()
     source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
     source "$(dirname "${BASH_SOURCE[0]}")/../attributes/default.bash"
 
-    installPortableBinary \
-        'ZDM-UTIL' \
-        "${ZDM_UTIL_DOWNLOAD_URL}" \
-        "${ZDM_UTIL_INSTALL_FOLDER_PATH}" \
-        'zdm-util' \
-        'version' \
-        'true'
+    header 'INSTALLING ZDM-UTIL'
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    umask '0022'
+    initializeFolder "${ZDM_UTIL_INSTALL_FOLDER_PATH}"
+    unzipRemoteFile "${ZDM_UTIL_DOWNLOAD_URL}" "${ZDM_UTIL_INSTALL_FOLDER_PATH}"
 }
 
 main "${@}"
