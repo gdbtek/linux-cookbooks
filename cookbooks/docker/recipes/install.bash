5c4f128eaeeacab6188ed029bbc1226bcfaced63
Nam Nguyen <gdbtek@yahoo.com>
2025-05-06 09:57:01 -0700

    update

--- cookbooks/docker/recipes/install.bash
+++ cookbooks/docker/recipes/install.bash
@@ -4,46 +4,9 @@ function install()
 {
     umask '0022'
 
-    # Download and Install
-
     checkExistURL "${DOCKER_DOWNLOAD_URL}"
     debug "\nDownloading '${DOCKER_DOWNLOAD_URL}'\n"
     curl -L "${DOCKER_DOWNLOAD_URL}" --retry 12 --retry-delay 5 | bash -e
-
-    # Config Grub
-
-    header 'UPDATING GRUB CONFIG'
-
-    local -r grubConfigFile='/etc/default/grub'
-    local -r grubConfigAttribute='GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"'
-
-    createFileFromTemplate \
-        "${grubConfigFile}" \
-        "${grubConfigFile}" \
-        'GRUB_CMDLINE_LINUX=""' "${grubConfigAttribute}"
-
-    appendToFileIfNotFound \
-        "${grubConfigFile}" \
-        "$(stringToSearchPattern "${grubConfigAttribute}")" \
-        "${grubConfigAttribute}" \
-        'true' \
-        'false' \
-        'true'
-
-    update-grub
-
-    # Display Status
-
-    statusService 'docker'
-
-    # Display Info
-
-    header 'DISPLAYING DOCKER INFO'
-    info "\n$(docker info)"
-
-    # Display Version
-
-    displayVersion "$(docker version)"
 
     umask '0077'
 }
