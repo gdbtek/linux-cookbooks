592d2ed19f2420f320d7863e5ebadf8a157da5ac
Nam Nguyen <gdbtek@yahoo.com>
2025-05-06 16:15:00 -0700

    bug fix

--- cookbooks/cortex-tools/recipes/install.bash
+++ cookbooks/cortex-tools/recipes/install.bash
@@ -5,13 +5,14 @@ function main()
     source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
     source "$(dirname "${BASH_SOURCE[0]}")/../attributes/default.bash"
 
-    installPortableBinary \
-        'CORTEX-TOOLS' \
-        "${CORTEX_TOOLS_DOWNLOAD_URL}" \
-        "${CORTEX_TOOLS_INSTALL_FOLDER_PATH}" \
-        'cortex-tools' \
-        'version' \
-        'true'
+    header 'INSTALLING CQLSH'
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    curl -fSL -o '/usr/bin/cortextool' "https://github.com/grafana/cortex-tools/releases/download/v0.11.3/cortextool_v0.11.3_Linux_x86_64"
+    chmod 755 '/usr/bin/cortextool'
+    cortexttool version
 }
 
 main "${@}"
