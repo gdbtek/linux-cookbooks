e306a834e3d781522ed48afa5fb5e13755a21ce9
Nam Nguyen <gdbtek@yahoo.com>
2025-05-06 16:26:02 -0700

    bug

--- cookbooks/cortex-tools/attributes/default.bash
+++ cookbooks/cortex-tools/attributes/default.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export CORTEX_TOOLS_DOWNLOAD_URL='https://github.com/grafana/cortex-tools/releases/download/v0.11.3/cortextool_v0.11.3_Linux_x86_6'
+export CORTEX_TOOLS_DOWNLOAD_URL='https://github.com/grafana/cortex-tools/releases/download/v0.11.3/cortextool_v0.11.3_Linux_x86_64'
