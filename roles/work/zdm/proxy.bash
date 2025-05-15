24de7d65993ebd4cc8ccdca7ee592c814dc5d29e
Nam Nguyen <gdbtek@yahoo.com>
2025-05-15 00:26:06 -0700

    update

---
+++ roles/work/zdm/proxy.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+function main()
+{
+    source "$(dirname "${BASH_SOURCE[0]}")/../../libraries/util.bash"
+
+    installPackages 'libselinux-python'
+
+    postUpMessage
+}
+
+main "${@}"
