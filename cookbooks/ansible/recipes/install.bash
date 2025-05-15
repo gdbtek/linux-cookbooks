c439e085fb205e957884cebe220cb393d1800495
Nam Nguyen <gdbtek@yahoo.com>
2025-05-15 10:47:11 -0700

    update

--- cookbooks/ansible/recipes/install.bash
+++ cookbooks/ansible/recipes/install.bash
@@ -3,6 +3,7 @@
 function main()
 {
     source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
+    source "$(dirname "${BASH_SOURCE[0]}")/../attributes/default.bash"
 
     header 'INSTALLING ANSIBLE'
 
