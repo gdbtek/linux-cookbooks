5d8a07996af5133e391fc87ffc27b5c38e60f2b4
Nam Nguyen <gdbtek@yahoo.com>
2025-05-14 23:51:09 -0700

    update

--- roles/work/zdm-proxy.bash
+++ roles/work/zdm-proxy.bash
@@ -11,7 +11,7 @@ function main()
     "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/zdm-proxy-automation/recipes/install.bash"
     "$(dirname "${BASH_SOURCE[0]}")/../../cookbooks/zdm-util/recipes/install.bash"
 
-    installPackages 'python3-jmespath'
+    installPackages 'libselinux-python' 'python3-jmespath'
     ansible-galaxy collection install 'community.docker'
     ansible-galaxy collection install 'community.general'
 
