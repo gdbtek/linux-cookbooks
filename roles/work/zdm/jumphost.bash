de2c3196b50ffc0de1a68f1092132eb84a76b148
Nam Nguyen <gdbtek@yahoo.com>
2025-05-16 15:47:06 -0700

    update

--- roles/work/zdm/jumphost.bash
+++ roles/work/zdm/jumphost.bash
@@ -5,9 +5,9 @@ function main()
     source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
 
     "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/ansible/recipes/install.bash"
-    # "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/cortex-tools/recipes/install.bash"
+    "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/cortex-tools/recipes/install.bash"
     "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/cqlsh/recipes/install.bash"
-    # "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/docker/recipes/install.bash"
+    "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/docker/recipes/install.bash"
     "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/zdm-proxy-automation/recipes/install.bash"
     "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/zdm-util/recipes/install.bash"
 
