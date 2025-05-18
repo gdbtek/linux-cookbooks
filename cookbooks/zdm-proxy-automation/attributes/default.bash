47dc8e177ff80305ea8f8bdc2a77b3765fd498d9
Nam Nguyen <gdbtek@yahoo.com>
2025-05-18 03:34:51 -0700

    Revert "update"
    This reverts commit 8daa71c93c1ce93d36d7409241e09b0447f21734.

---
+++ cookbooks/zdm-proxy-automation/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+export ZDM_PROXY_AUTOMATION_DOWNLOAD_URL='https://github.com/datastax/zdm-proxy-automation/archive/refs/tags/v2.3.1.tar.gz'
+
+export ZDM_PROXY_AUTOMATION_INSTALL_FOLDER_PATH='/opt/zdm-proxy-automation'
---
+++ cookbooks/zdm-proxy-automation/recipes/install.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+function main()
+{
+    source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
+    source "$(dirname "${BASH_SOURCE[0]}")/../attributes/default.bash"
+
+    header 'INSTALLING ZDM-PROXY-AUTOMATION'
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    umask '0022'
+    initializeFolder "${ZDM_PROXY_AUTOMATION_INSTALL_FOLDER_PATH}"
+    unzipRemoteFile "${ZDM_PROXY_AUTOMATION_DOWNLOAD_URL}" "${ZDM_PROXY_AUTOMATION_INSTALL_FOLDER_PATH}"
+    ls -la "${ZDM_PROXY_AUTOMATION_INSTALL_FOLDER_PATH}"
+    umask '0077'
+}
+
+main "${@}"
