89b65ab5f61544512677e1d2cd848a0181dcf5c4
Nam Nguyen <gdbtek@yahoo.com>
2025-05-18 08:21:17 -0700

    update

--- roles/work/zdm/zdm-proxy.bash
+++ roles/work/zdm/zdm-proxy.bash
@@ -4,7 +4,7 @@ function main()
 {
     source "$(dirname "${BASH_SOURCE[0]}")/../../../libraries/util.bash"
 
-    apt install -y 'curl' 'software-properties-common' 'python3-pip' 'virtualenv' 'python3-setuptools'
+    apt-get install -y 'curl' 'software-properties-common' 'python3-pip' 'virtualenv' 'python3-setuptools'
 
     "$(dirname "${BASH_SOURCE[0]}")/../../../cookbooks/docker/recipes/install.bash"
 
