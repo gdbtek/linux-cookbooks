b7fc5b975d3714ade1ea56bd5ed732704ab6b46e
Nam Nguyen <gdbtek@yahoo.com>
2025-07-09 13:41:19 -0700

    update version

--- cookbooks/test-ssl/attributes/default.bash
+++ cookbooks/test-ssl/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TEST_SSL_DOWNLOAD_URL='https://codeload.github.com/drwetter/testssl.sh/tar.gz/v3.2.1'
+export TEST_SSL_DOWNLOAD_URL='https://github.com/testssl/testssl.sh/archive/refs/tags/v3.2.1.tar.gz'
 
 export TEST_SSL_INSTALL_FOLDER_PATH='/opt/test-ssl'
