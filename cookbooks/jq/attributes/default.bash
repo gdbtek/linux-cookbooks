86c82509a539657bf31c744966cf96e8349f2fbc
Nam Nguyen <gdbtek@yahoo.com>
2025-07-18 09:49:15 -0700

    update

--- cookbooks/jq/attributes/default.bash
+++ cookbooks/jq/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export JQ_DOWNLOAD_URL='https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-i386'
+export JQ_DOWNLOAD_URL='https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-i386'
 
 export JQ_INSTALL_FOLDER_PATH='/opt/jq'
